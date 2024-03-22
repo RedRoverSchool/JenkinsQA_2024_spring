18e803e4962c4f8324cdc20fdfc95f4d89e8d6fb
Yakov Tereshenkov <tereshenkov@gmail.com>
2024-03-22 19:39:33 +0300

    Refactor the first test

--- src/test/java/school/redrover/UnderdogsGroupTest.java
+++ src/test/java/school/redrover/UnderdogsGroupTest.java
@@ -12,12 +12,13 @@ public class UnderdogsGroupTest {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://demoqa.com/");
+        driver.findElement(By.id("close-fixedban")).click();
         driver.findElement(By.xpath("//div[@class='category-cards']/div[1]")).click();
         driver.findElement(By.xpath("//span[text()='Text Box']")).click();
         String name = "test";
         driver.findElement(By.id("userName")).sendKeys(name);
         driver.findElement(By.id("submit")).click();
-        String result = driver.findElement(By.xpath("//p[@id='name']")).getText();
+        String result = driver.findElement(By.id("name")).getText();
 
         Assert.assertEquals(result, "Name:" + name);
 
