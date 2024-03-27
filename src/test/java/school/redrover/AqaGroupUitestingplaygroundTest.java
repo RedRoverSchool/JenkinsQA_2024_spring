755cd046750d80cd6f3ba26f0ad04bd9e2577ce1
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-03-27 21:25:12 +0300

    [!] testOverlappedElement

--- src/test/java/school/redrover/AqaGroupUitestingplaygroundTest.java
+++ src/test/java/school/redrover/AqaGroupUitestingplaygroundTest.java
@@ -1,19 +1,13 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
-
-public class AqaGroupUitestingplaygroundTest extends AqaGroupBaseTest{
+public class AqaGroupUitestingplaygroundTest extends AqaGroupBaseTest {
     @Test
     public void testClientSideDelay() {
         getDriver().get("http://uitestingplayground.com/clientdelay");
@@ -30,20 +24,15 @@ public class AqaGroupUitestingplaygroundTest extends AqaGroupBaseTest{
 
     @Test
     public void testOverlappedElement() {
-        String link = "http://uitestingplayground.com/overlapped";
-        WebDriver driver = new ChromeDriver();
+        getDriver().get("http://uitestingplayground.com/overlapped");
 
-        try {
-            driver.get(link);
+        final String myName = "myName";
+        WebElement name = scrollIntoView(getDriver().findElement(By.id("name")));
+        name.sendKeys(myName);
 
-            WebElement name = driver.findElement(By.id("name"));
-            ((JavascriptExecutor) driver).executeScript("return arguments[0].scrollIntoView(true);", name);
-            String myName = "myName";
-            name.sendKeys(myName);
-
-            Assert.assertEquals(name.getAttribute("value"), myName, "input field contains wrong value");
-        } finally {
-            driver.quit();
-        }
+        Assert.assertEquals(
+                name.getAttribute("value"),
+                myName,
+                "input field contains wrong value");
     }
 }
