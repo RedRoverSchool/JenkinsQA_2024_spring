051c10dad2c23e62bc0e3530dea27ad6f37a9c15
DinaSizyuk <patatinka5@gmail.com>
2024-03-30 21:48:12 +0300

    Add testFindArticle

--- src/test/java/school/redrover/GroupJavaExitCodeZeroTest.java
+++ src/test/java/school/redrover/GroupJavaExitCodeZeroTest.java
@@ -1,7 +1,7 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -9,6 +9,13 @@ import school.redrover.runner.BaseTest;
 public class GroupJavaExitCodeZeroTest extends BaseTest {
 
     private static final String BASE_URL = "http://automationexercise.com";
+
+    private final static String HABR_URL = "https://habr.com/ru/search/";
+
+    private final static String TEST_TEXT = "RedRover School";
+
+    private final static String expectedResult = "Большая подборка ресурсов и сообществ для тестировщика";
+
 
     @Test
     public void testAllProductsNavigation() throws InterruptedException {
@@ -37,5 +44,25 @@ public class GroupJavaExitCodeZeroTest extends BaseTest {
         final String actualHeader = getDriver().findElement(By.xpath("//div[@class = 'features_items']/h2")).getText();
 
         Assert.assertEquals(actualHeader, expectedHeader);
+    }
+
+    @Test
+    public void testFindArticle () throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(HABR_URL);
+        driver.manage().window().maximize();
+
+        WebElement textArea = driver.findElement(By.xpath("//input[@name='q']"));
+        textArea.sendKeys(TEST_TEXT);
+        textArea.sendKeys(Keys.RETURN);
+        Thread.sleep(2000);
+        String hrefXpath = "//article[@id='720526']/div[1]/h2/a[@href ='/ru/articles/720526/'][@class='tm-title__link']";
+        WebElement firstArticle = driver.findElement(By.xpath(hrefXpath));
+        String actualResult = firstArticle.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
     }
 }
