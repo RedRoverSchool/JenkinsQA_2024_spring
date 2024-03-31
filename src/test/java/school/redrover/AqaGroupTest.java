2bf58d761c070862a45b6b496ef13c722e7f48f2
iKuper <amalgammy@mail.ru>
2024-03-31 17:01:14 +0300

    moving tests from AqaGroupUitestingplaygroundTest

--- src/test/java/school/redrover/AqaGroupTest.java
+++ src/test/java/school/redrover/AqaGroupTest.java
@@ -779,4 +779,31 @@ public class AqaGroupTest extends AqaGroupBaseTest {
                 "You shall not pass");
     }
 
+    @Test
+    public void testClientSideDelay() {
+        getDriver().get("http://uitestingplayground.com/clientdelay");
+
+        getWait25().until(ExpectedConditions.elementToBeClickable(By.id("ajaxButton"))).click();
+
+        Assert.assertTrue(
+                getWait25()
+                        .until(ExpectedConditions.visibilityOfElementLocated(By.className("bg-success")))
+                        .getText()
+                        .startsWith("Data calculated"),
+                "Label text wrong");
+    }
+
+    @Test
+    public void testOverlappedElement() {
+        getDriver().get("http://uitestingplayground.com/overlapped");
+
+        final String myName = "myName";
+        WebElement name = scrollIntoView(getDriver().findElement(By.id("name")));
+        name.sendKeys(myName);
+
+        Assert.assertEquals(
+                name.getAttribute("value"),
+                myName,
+                "input field contains wrong value");
+    }
 }
