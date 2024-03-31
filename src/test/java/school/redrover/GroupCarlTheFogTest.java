7a2a20445eb6405eec35506519fbe61d3bffe1b1
Stanislav Kucher <kucher.stanislav94@gmail.com>
2024-03-30 20:28:25 -0700

    A little bit changes

--- src/test/java/school/redrover/GroupCarlTheFogTest.java
+++ src/test/java/school/redrover/GroupCarlTheFogTest.java
@@ -1,12 +1,14 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GroupCarlthefogTest {
+public class GroupCarlTheFogTest {
 
     @Test
     public void testSaucedemo() {
@@ -21,6 +23,29 @@ public class GroupCarlthefogTest {
         String actualPageName = driver.findElement(By.xpath("//div[@class= 'app_logo']")).getText();
 
         Assert.assertEquals(expectedPageName, actualPageName);
+        driver.quit();
+    }
+
+    @Test
+    public void newTest(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com");
+
+        WebElement fieldLogin = driver.findElement(By.name("user-name"));
+        fieldLogin.sendKeys("standard_user");
+
+        WebElement fieldPassword = driver.findElement(By.name("password"));
+        fieldPassword.sendKeys("secret_sauce");
+
+        WebElement button = driver.findElement(By.id("login-button"));
+        button.click();
+
+        WebElement message = driver.findElement(By.className("app_logo"));
+        String value = message.getText();
+
+        Assert.assertEquals(value, "Swag Labs");
+
         driver.quit();
     }
 }
