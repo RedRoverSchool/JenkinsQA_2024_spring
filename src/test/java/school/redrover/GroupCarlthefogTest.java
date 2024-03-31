76c4f1fe4ed9025532dca8c115fcd96b501e4d1c
Marina <marinam026@gmail.com>
2024-03-30 21:34:47 -0400

    Moving test to group folder

--- src/test/java/school/redrover/GroupCarlthefogTest.java
+++ src/test/java/school/redrover/GroupCarlthefogTest.java
@@ -174,4 +174,26 @@ public class GroupCarlthefogTest extends BaseTest {
 
         Assert.assertEquals(value, "Congrats! Your order of $905.99 has been registered!");
     }
+    @Test
+    public void tangaTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.tanga.com/");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+
+        WebElement textBox = driver.findElement(By.name("q"));
+        textBox.sendKeys("5959595959");
+//        textBox.sendKeys(Keys.ENTER);
+
+        WebElement button = driver.findElement(By.xpath("//button[@aria-label='Search']"));
+        button.click();
+
+        WebElement message = driver.findElement(By.xpath("//h3"));
+        String value = message.getText();
+
+        Assert.assertEquals("No products found", value);
+
+        driver.quit();
+
+    }
 }
