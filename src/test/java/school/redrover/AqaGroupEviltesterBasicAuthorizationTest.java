9ffc5d112893fb6586b450a585108ed9fb3dfce6
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-03-31 03:55:47 +0300

    [-] empty class

--- src/test/java/school/redrover/AqaGroupEviltesterBasicAuthorizationTest.java
+++ src/test/java/school/redrover/AqaGroupEviltesterBasicAuthorizationTest.java
@@ -7,11 +7,5 @@ import org.testng.annotations.Test;
 
 public class AqaGroupEviltesterBasicAuthorizationTest extends AqaGroupBaseTest {
 
-    @Test
-    public void testBasicAuthorization() {
-        getDriver().get("https://authorized:password001@testpages.eviltester.com/styled/auth/basic-auth-results.html");
-        Assert.assertEquals(
-                getDriver().findElement(By.id("status")).getText(),
-                "Authenticated");
-    }
+
 }
