6ee6d6d5a31223b04291ad0fd27c475a7f54b9f0
Iryna <iryna.kolhanova@gmail.com>
2024-03-30 23:14:48 -0400

    fix import

--- src/test/java/school/redrover/GroupHollyGuacamoleTest.java
+++ src/test/java/school/redrover/GroupHollyGuacamoleTest.java
@@ -1,7 +1,7 @@
 package school.redrover;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
