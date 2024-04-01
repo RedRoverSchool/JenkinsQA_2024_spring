750edf8f0f972215e2340d82857fdbc37453e704
AnastasiaSerova1 <anastasia.srv@gmailcom>
2024-04-01 13:52:12 -0700

    Test Script for Wikipedia from Anastasia Serova

--- src/test/java/school/redrover/GroupCarlthefogTest.java
+++ src/test/java/school/redrover/GroupCarlthefogTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -13,6 +15,8 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+
 
 public class GroupCarlthefogTest extends BaseTest {
 
@@ -174,4 +178,55 @@ public class GroupCarlthefogTest extends BaseTest {
 
         Assert.assertEquals(value, "Congrats! Your order of $905.99 has been registered!");
     }
+
+
+    private WebDriver driver;
+
+        @BeforeClass
+        public void setUp() {
+            // Установка пути к драйверу Chrome WebDriver
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\anast\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+            // Инициализация экземпляра WebDriver
+            driver = new ChromeDriver();
+        }
+
+        @Test
+        public void testWikipediaSearch() {
+            // Открыть страницу Wikipedia
+            driver.get("https://en.wikipedia.org");
+
+            // Найти поле ввода для поиска
+            WebElement searchBox = driver.findElement(By.id("searchInput"));
+
+            // Ввести запрос в поле поиска
+            searchBox.sendKeys("Selenium (software)");
+
+            // Найти кнопку отправки формы по текстовому содержимому
+            WebElement submitButton = driver.findElement(By.xpath("//button[contains(@class, 'search-input')]"));
+
+            // Нажать на кнопку отправки формы для выполнения поиска
+            submitButton.click();
+
+            // Подождать некоторое время для загрузки результатов поиска
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Проверить заголовок страницы
+            String expectedTitle = "Selenium (software) - Wikipedia";
+            String actualTitle = driver.getTitle();
+            System.out.println("Page title is: " + actualTitle);
+            // Проверка совпадения заголовка страницы с ожидаемым значением
+            Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match expected title");
+        }
+
+        @AfterClass
+        public void tearDown() {
+            // Закрыть браузер после выполнения всех тестов
+            driver.quit();
+        }
+
+
 }
