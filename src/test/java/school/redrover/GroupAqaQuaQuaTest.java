c0faaac466e17ddcc032c0512b60b5159556653e
Dr.Johanson <doc.johanson@gmail.com>
2024-04-01 20:29:55 +0500

    add the-internet test to Group class

--- src/test/java/school/redrover/GroupAqaQuaQuaTest.java
+++ src/test/java/school/redrover/GroupAqaQuaQuaTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.testng.Assert;
@@ -32,6 +33,26 @@ public class GroupAqaQuaQuaTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.className("product-name")).getText(), "Computing and Internet");
 
+    }
+    @Test
+    public void testLoginTheInternet() {
+        WebDriver driver = getDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        Assert.assertEquals(driver.getTitle(), "The Internet");
+
+        driver.findElement(By.linkText("Form Authentication")).click();
+
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button")).click();
+
+        Assert.assertEquals(driver.findElement(By.tagName("h2")).getText(), "Secure Area");
+//
+//        driver.findElement(By.linkText("Logout")).click();
+//        driver.findElement(By.tagName("h2")).getText();
+//
+//        Assert.assertEquals(driver.findElement(By.tagName("h2")).getText(), "Login Page");
     }
 }
 
