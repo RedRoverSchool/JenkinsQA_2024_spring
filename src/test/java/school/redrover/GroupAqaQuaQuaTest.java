2f84cb3c316b794b3f7718173bc5cc0266657030
LizaBazarova <bazarova87@rambler.ru>
2024-04-01 23:52:14 +0500

    Add new tests

--- src/test/java/school/redrover/GroupAqaQuaQuaTest.java
+++ src/test/java/school/redrover/GroupAqaQuaQuaTest.java
@@ -2,8 +2,10 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -48,6 +50,56 @@ public class GroupAqaQuaQuaTest extends BaseTest {
                 By.cssSelector("div.page.category-page>div.page-title>h1")
                 ));
         Assert.assertEquals(menuOnPageGiftCards.getText(), "Gift Cards");
+    }
+    @Test
+    public void testComputersSearch() throws InterruptedException {
+        getDriver().get("https://demowebshop.tricentis.com/");
+
+        WebElement text = getDriver().findElement(By.id("small-searchterms"));
+        text.sendKeys("computer");
+
+        Thread.sleep(1000);
+
+        WebElement button = getDriver().findElement(By.xpath("//input[@class='button-1 search-box-button']"));
+        button.click();
+        Thread.sleep(2000);
+
+        WebElement SearchKeyword = getDriver().findElement(By.cssSelector(".page-title"));
+        String resultText = SearchKeyword.getText();
+
+        Assert.assertEquals(resultText, "Search");
+    }
+
+    @Test
+    public void testOpeningComputersPage() throws InterruptedException {
+        getDriver().get("https://demowebshop.tricentis.com/");
+
+        WebElement button = getDriver().findElement(By.xpath("//a[@href = '/computers']"));
+        button.click();
+        Thread.sleep(2000);
+
+        WebElement SearchKeyword = getDriver().findElement(By.cssSelector(".page-title"));
+        String resultText = SearchKeyword.getText();
+
+        Assert.assertEquals(resultText, "Computers");
+    }
+
+    @Test
+    public void testOpeningDesktopsPage() throws InterruptedException {
+        getDriver().get("https://demowebshop.tricentis.com/");
+
+        WebElement button = getDriver().findElement(By.xpath("//ul[@class='list' ]//li[@class='inactive']//a[@href='/computers']"));
+        button.click();
+        Thread.sleep(2000);
+
+        WebElement button1 = getDriver().findElement(By.xpath("//ul[@class='sublist' ]//li[@class='inactive']//a[@href = '/desktops']"));
+        button1.click();
+        Thread.sleep(2000);
+
+        WebElement SearchKeyword = getDriver().findElement(By.cssSelector(".page-title"));
+        String resultText = SearchKeyword.getText();
+
+        Assert.assertEquals(resultText, "Desktops");
     }
 }
 
