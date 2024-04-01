5f7cf47df88d3759873cf5fb280d649c847c29a3
Dr.Johanson <doc.johanson@gmail.com>
2024-04-01 20:41:23 +0500

    remove unused lines

--- src/test/java/school/redrover/GroupAqaQuaQuaTest.java
+++ src/test/java/school/redrover/GroupAqaQuaQuaTest.java
@@ -1,15 +1,10 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-
 
 public class GroupAqaQuaQuaTest extends BaseTest {
 
