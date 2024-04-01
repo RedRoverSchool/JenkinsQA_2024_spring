acd03e0d4e24984a3ba9b6b12ff517d3b3db8482
Elena Vasyur <elena.vasyur@gmail.com>
2024-04-01 01:39:56 -0400

    First test pull request

--- src/test/java/school/redrover/Elena_test1.java
+++ src/test/java/school/redrover/Elena_test1.java
@@ -2,6 +2,6 @@ package school.redrover;
 
 public class Elena_test1 {
     public static void main(String[] args) {
-        System.out.println("My first pull request");
+        System.out.println("My first pull request ");
     }
 }
