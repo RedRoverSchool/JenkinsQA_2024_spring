9a3f0762beb998911cbc2a551c6c20bb601e6844
Dr.Johanson <doc.johanson@gmail.com>
2024-04-01 20:49:11 +0500

    Removed unused lines

--- src/test/java/school/redrover/GroupAqaQuaQuaTest.java
+++ src/test/java/school/redrover/GroupAqaQuaQuaTest.java
@@ -1,9 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
