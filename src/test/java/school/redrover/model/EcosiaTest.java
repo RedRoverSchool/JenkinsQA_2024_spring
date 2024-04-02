4d9b6cee37ff88b95300f742120d86baa9120378
edit-test <vgrushenko3@gmail.com>
2024-04-01 21:56:43 -0400

    change request

---
+++ src/test/java/school/redrover/model/EcosiaTest.java
@@ -0,0 +1,29 @@
+package school.redrover.model;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class EcosiaTest {
+    @Test
+    public void testGoodLuck() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ecosia.org/");
+
+        WebElement form = driver.findElement(By.className("search-form__input"));
+        form.sendKeys("weather in Miami");
+
+        WebElement search = driver.findElement(By.xpath("//button[@data-test-id='search-form-submit']"));
+        search.click();
+
+        WebElement menu = driver.findElement(By.linkText("More"));
+        menu.click();
+
+        driver.quit();
+
+
+
+    }
+}
