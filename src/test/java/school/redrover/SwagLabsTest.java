a403cf25d4653a1d40ef277719c3cc0581f1aa7a
Anzhelika1202 <kulyani.74@gmail.com>
2024-04-02 17:08:25 -0700

    change my test

--- src/test/java/school/redrover/SwagLabsTest.java
+++ src/test/java/school/redrover/SwagLabsTest.java
@@ -10,27 +10,23 @@ import org.testng.annotations.Test;
 
 public class SwagLabsTest {
     @Test
-    public void testUsps() throws InterruptedException {
+    public void testUsps() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/v1/");
-
 
         WebElement userNameField = driver.findElement(By.id("user-name"));
         userNameField.sendKeys("standard_user");
 
-
         WebElement passwordField = driver.findElement(By.id("password"));
         passwordField.sendKeys("secret_sauce");
-
 
         WebElement buttonLogin = driver.findElement(By.id("login-button"));
         buttonLogin.click();
 
-
         WebElement findText = driver.findElement(By.xpath("//div[@class='product_label']"));
         String textString = findText.getText();
-        Assert.assertEquals(textString, "Products");
 
+        Assert.assertEquals(textString, "Products");
 
         driver.quit();
     }
