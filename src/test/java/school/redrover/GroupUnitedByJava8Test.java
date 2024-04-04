a6fcd79c995568f0b8f9fd90bd06fce5297b07db
Pavel Pugach <PPugach@qulix.com>
2024-04-04 13:08:11 +0300

    исправление замечаний с ревью

--- src/test/java/school/redrover/GroupUnitedByJava8Test.java
+++ src/test/java/school/redrover/GroupUnitedByJava8Test.java
@@ -463,43 +463,29 @@ public class GroupUnitedByJava8Test extends BaseTest {
     }
 
     @Test
-    public void testSaucedemoUsernameTextByDefault() {
+    public void testSaucedemoPlaceholderUsernameByDefault() {
         getDriver().get("https://www.saucedemo.com/");
 
-        Assert.assertEquals(
-            getDriver().
-                findElement(
-                    By.name("user-name")).
-                getAttribute("placeholder"), "Username");
+        Assert.assertEquals(getDriver().findElement(By.name("user-name")).getAttribute("placeholder"), "Username");
     }
 
     @Test
-    public void testSaucedemoPasswordTextByDefault() {
+    public void testSaucedemoPasswordPlaceholderByDefault() {
         getDriver().get("https://www.saucedemo.com/");
 
-        Assert.assertEquals(
-            getDriver().
-                findElement(
-                    By.name("password")).
-                getAttribute("placeholder"), "Password");
+        Assert.assertEquals(getDriver().findElement(By.name("password")).getAttribute("placeholder"), "Password");
     }
 
     @Test
     public void testSaucedemoErrorLoginWithEmptyInputs() {
         getDriver().get("https://www.saucedemo.com/");
         getDriver().findElement(By.id("login-button")).click();
-        WebElement emptyLoginError = getDriver().findElement(
-            By.xpath("//div/h3[@data-test='error']"));
+        WebElement emptyLoginError = getDriver().findElement(By.xpath("//div/h3[@data-test='error']"));
         emptyLoginError.isDisplayed();
         Assert.assertEquals(emptyLoginError.getText(), "Epic sadface: Username is required");
 
-        getDriver().findElement(
-                By.xpath("(//div[@class='form_group']/*)[2]")).
-            isDisplayed();
-        //както иначе не смог найти элементы на странице
-        getDriver().findElement(
-                By.xpath("(//div[@class='form_group']/*)[4]")).
-            isDisplayed();
+        getDriver().findElement(By.xpath("(//div[@class='form_group']/*)[2]")).isDisplayed();
+        getDriver().findElement(By.xpath("(//div[@class='form_group']/*)[4]")).isDisplayed();
     }
 
     @Test
@@ -509,8 +495,7 @@ public class GroupUnitedByJava8Test extends BaseTest {
         getDriver().findElement(By.xpath("//div/h3[@data-test='error']")).isDisplayed();
         getDriver().findElement(By.className("error-button")).click();
 
-        boolean result = getDriver().findElements(By.xpath("//div/h3[@data-test='error']"))
-            .isEmpty();
+        boolean result = getDriver().findElements(By.xpath("//div/h3[@data-test='error']")).isEmpty();
         Assert.assertTrue(result);
     }
 
