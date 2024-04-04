1d09f52dd5d075ba7091243ec158deb4a77dbffd
Valentin Podkova <val.podkova@gmail.com>
2024-04-04 21:25:58 +0300

    rename test method and fixed my test

--- src/test/java/school/redrover/GuruTest.java
+++ src/test/java/school/redrover/GuruTest.java
@@ -2,36 +2,26 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.time.Duration;
+
 public class GuruTest extends BaseTest {
     @Test
-    public void FormTest() {
+    public void testLoginGuru() {
 
         WebDriver driver = getDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         getDriver().get("https://demo.guru99.com/test/login.html");
 
         getDriver().findElement(By.id("email")).sendKeys("abcd@gmail.com");
         getDriver().findElement(By.name("passwd")).sendKeys("abcdefghlkjl");
         getDriver().findElement(By.id("SubmitLogin")).click();
 
-        driver.quit();
-    }
-
-    @Test
-    public void GuruRadioTest() {
-        WebDriver driver = getDriver();
-        getDriver().get("https://demo.guru99.com/test/radio.html");
-
-        getDriver().findElement (By.xpath("//input[@value='Option 1']")).click();
-        getDriver().findElement(By.xpath("//input[@value='Option 2']")).click();
-        getDriver().findElement(By.xpath("//input[@value='Option 3']")).click();
-        getDriver().findElement(By.xpath("//input[@value='checkbox1']")).click();
-        getDriver().findElement(By.xpath("//input[@value='checkbox2']")).click();
-        getDriver().findElement(By.xpath("//input[@value='checkbox3']")).click();
-
-        driver.quit();
+        Assert.assertEquals(driver.findElement(By.xpath("//h3[normalize-space()='Successfully Logged in...']")).getText(),
+                "Successfully Logged in...");
     }
 }
 
