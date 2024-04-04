f4b8a18601bf1f04bbc6fd34d3f7a3198663f24e
Dziakonski <dziakonski@gmail.com>
2024-04-05 00:03:42 +0300

    Add test new

--- src/test/java/school/redrover/CamelCaseGroupTest.java
+++ src/test/java/school/redrover/CamelCaseGroupTest.java
@@ -1,8 +1,6 @@
 package school.redrover;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -60,5 +58,36 @@ public class CamelCaseGroupTest extends BaseTest {
 
         String currentURL = getDriver().getCurrentUrl();
         Assert.assertEquals(currentURL, "https://www.wildberries.ru/catalog/0/search.aspx?search=177390212");
+    }
+
+    @Test
+    public void testOnliner() throws InterruptedException {
+
+        getDriver().get("https://www.onliner.by/");
+        getDriver().manage().window().maximize();
+
+        WebElement linkSmart = getDriver().findElement(By.className("project-navigation__sign"));
+        linkSmart.click();
+
+        WebElement linkHonor = getDriver().findElement(By.xpath("(//a[@href = 'https://catalog.onliner.by/mobile/honor/x9b8256glmb'])[2]"));
+        linkHonor.click();
+
+        WebElement buttonOffer = getDriver().findElement(By.cssSelector("a.button:nth-child(1) > span:nth-child(1)"));
+        buttonOffer.click();
+
+        WebElement buttonFilterDropdown = getDriver().findElement(By.className("input-style__real"));
+        buttonFilterDropdown.click();
+
+        WebElement textFilterDropdown = getDriver().findElement(By.xpath("//select/option[2]"));
+        textFilterDropdown.click();
+
+        WebElement buttonPay = getDriver().findElement(By.xpath("(//a[@href='https://cart.onliner.by/?action=buyNow&shopId=707&productKey=x9b8256glmb&productId=4246922&positionId=707:4246922001&deliveryType=courier'])[2]"));
+        buttonPay.click();
+
+        Thread.sleep(5000);
+
+        WebElement message = getDriver().findElement(By.xpath("//*[@class='cart-form__title cart-form__title_base cart-form__title_nocondensed cart-form__title_condensed-other']"));
+
+        Assert.assertEquals(message.getText(), "Оформление заказа");
     }
 }
