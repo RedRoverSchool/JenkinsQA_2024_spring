a02d34cbdac6cd077c931085d3cef5ac6dd5f1ef
Sergey.D <sergei.demyanenko@gmail.com>
2024-04-04 21:44:07 -0400

    First commit

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -1,24 +1,80 @@
 package school.redrover.runner;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
+@Listeners({FilterForTests.class})
 public abstract class BaseTest {
 
     private WebDriver driver;
 
+    private void startDriver() {
+        ProjectUtils.log("Browser open");
+
+        driver = ProjectUtils.createDriver();
+    }
+
+    private void clearData() {
+        ProjectUtils.log("Clear data");
+        JenkinsUtils.clearData();
+    }
+
+    private void loginWeb() {
+        ProjectUtils.log("Login");
+        JenkinsUtils.login(driver);
+    }
+
+    private void getWeb() {
+        ProjectUtils.log("Get web page");
+        ProjectUtils.get(driver);
+    }
+
+    private void stopDriver() {
+        try {
+            JenkinsUtils.logout(driver);
+        } catch (Exception ignore) {}
+
+        closeDriver();
+    }
+
+    private void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+
+            driver = null;
+
+            ProjectUtils.log("Browser closed");
+        }
+    }
+
     @BeforeMethod
     protected void beforeMethod(Method method) {
-        driver = new ChromeDriver();
+        ProjectUtils.logf("Run %s.%s", this.getClass().getName(), method.getName());
+        try {
+            clearData();
+            startDriver();
+            getWeb();
+            loginWeb();
+        } catch (Exception e) {
+            closeDriver();
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterMethod
-    protected void afterMethod(Method method) {
-        driver.quit();
+    protected void afterMethod(Method method, ITestResult testResult) {
+        if (!ProjectUtils.closeBrowserIfError()) {
+            stopDriver();
+        }
+
+        ProjectUtils.logf("Execution time is %o sec\n\n", (testResult.getEndMillis() - testResult.getStartMillis()) / 1000);
     }
 
     protected WebDriver getDriver() {
