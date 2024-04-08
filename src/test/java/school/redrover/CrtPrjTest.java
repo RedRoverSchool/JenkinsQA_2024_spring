0d87fd57377256016af1d2905bbf10f20a8b3b48
Kuba Bekoff <k.bekoff.5@gmail.com>
2024-04-09 01:33:21 +0600

    Create,check and configure proj.

--- src/test/java/school/redrover/CrtPrjTest.java
+++ src/test/java/school/redrover/CrtPrjTest.java
@@ -9,22 +9,23 @@ import school.redrover.runner.BaseTest;
 public class CrtPrjTest extends BaseTest {
     @Test
 
-    public void testDeleteProject() {
+    public void testCrtProject() {
         String text;
 
         getDriver().findElement(By.className("trailing-icon")).click();
         getDriver().findElement(By.id("name")).sendKeys("Freestyle");
-        getDriver().findElement(By.xpath("//*[@id=" +
-                "\"j-add-item-type-standalone-projects\"]/ul/li[1]/label/span")).click();
+        getDriver().findElement(By.xpath(
+                "//*[contains(text(),'Freestyle project')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
 
         getDriver().findElement(By.xpath(
-                "/html/body/div[1]/ol/li[1]/a")).click();
-        getDriver().findElement(By.id("search-box")).sendKeys("Freestyle", Keys.ENTER);
+                "//*[@id='breadcrumbs']/li[1]/a")).click();
+        getDriver().findElement(By.id("search-box")).sendKeys(
+                "Freestyle", Keys.ENTER);
 
         getDriver().findElement(By.xpath(
-                "//*[@id=\"tasks\"]/div[5]/span/a/span[1]")).click();
+                "//*[@href='/job/Freestyle/configure']")).click();
         text = getDriver().findElement(By.xpath("//h2")).getText();
 
         Assert.assertEquals(text,"General");
