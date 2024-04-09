f376faefecdd1cd8631e1715d79a080308abc538
Evgeniy <yndrab@yandex.ru>
2024-04-09 11:40:08 +0400

    add-jenkins-test

--- src/test/java/school/redrover/DraftJenkinsTest.java
+++ src/test/java/school/redrover/DraftJenkinsTest.java
@@ -8,6 +8,13 @@ import school.redrover.runner.BaseTest;
 public class DraftJenkinsTest extends BaseTest {
 
     @Test
+    public void testElementPeople() {
+        getDriver().findElement(By.xpath("//a[@href='/asynchPeople/']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class='jenkins-app-bar__content']")).getText(), "People");
+    }
+
+    @Test
     public void testElementWelcome() {
 
         Assert.assertEquals(getDriver().findElement(By.cssSelector("div h1")).getText(), "Welcome to Jenkins!");
