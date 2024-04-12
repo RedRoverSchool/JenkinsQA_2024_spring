e6717f82cd685c12a0635c7c4cedf2f6513e4df3
Tetiana Rakhmanova <tetiana_rakhmanova@epam.com>
2024-04-12 13:59:36 -0700

    Add new test TR

---
+++ src/test/java/school/redrover/StartBuildingTest.java
@@ -0,0 +1,16 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class StartBuildingTest extends BaseTest {
+    @Test
+    public  void testStartBuilding() {
+        WebElement startBuilding = getDriver().findElement(By.xpath("//h2[text()='Start building your software project']"));
+
+        Assert.assertEquals(startBuilding.getText(), "Start building your software project");
+    }
+}
