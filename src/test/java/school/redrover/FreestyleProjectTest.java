5482bcc053588cd3aa074ddc87806c04da90209f
MariJern <marina.jtest@gmail.com>
2024-04-12 23:03:10 -0500

    Rename CreateFreestyleProjectTest

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class CreateFreestyleProjectTest extends BaseTest {
+public class FreestyleProjectTest extends BaseTest {
 
     @Test
     public void testCreateFreestyleProject() {
