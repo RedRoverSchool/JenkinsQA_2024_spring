3153ab69e8114bb07e0323cfb588dfce99ce8e10
natalliach040478 <n.chalavechkina@gmail.com>
2024-04-13 20:20:26 +0200

    Create testFreestyleProjectMoveToFolder()

--- src/test/java/school/redrover/OperationJavaGroupTest.java
+++ src/test/java/school/redrover/OperationJavaGroupTest.java
@@ -7,6 +7,11 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class OperationJavaGroupTest extends BaseTest {
+    public WebElement okButton(){
+        return getDriver().findElement(By.id("ok-button"));
+    }
+    public WebElement submitButton(){
+        return getDriver().findElement(By.xpath("//button[@name = 'Submit']"));}
 
     @Test
     public void testFreestyleProject() {
@@ -32,6 +37,33 @@ public class OperationJavaGroupTest extends BaseTest {
 
         Assert.assertEquals(nameDashboard.getText(), "FreestyleProject");
     }
-}
 
+    @Test
+    public void testFreestyleProjectMoveToFolder() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']"))
+               .sendKeys("Folder");
+        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
+        okButton().click();
+        submitButton().click();
+
+        getDriver().findElement(By.xpath("//ol/li/a[@href='/']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']"))
+                .sendKeys("FreestyleProject");
+        getDriver().findElement(By.xpath("//span[contains(text(), 'Freestyle project')]")).click();
+        okButton().click();
+        submitButton().click();
+
+        getDriver().findElement(By.xpath("//ol/li/a[@href='/']")).click();
+        getDriver().findElement(By.xpath("//span[contains(text(), 'FreestyleProject')]")).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/FreestyleProject/move']")).click();
+        getDriver().findElement(By.xpath("//select/option[@value='/Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
+
+        getDriver().findElement(By.xpath("//ol/li/a[@href='/']")).click();
+        getDriver().findElement(By.xpath("//td/a[@href='job/Folder/']")).click();
+    }
+}
 
