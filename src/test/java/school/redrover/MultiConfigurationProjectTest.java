b7eb0f714b9e471f6dbab3cb7e776fd5f8123b98
iKuper <amalgammy@mail.ru>
2024-04-14 22:15:37 +0300

    TC_03.001.02| Multi-configuration project > Edit description on the status page

--- src/test/java/school/redrover/MultiConfigurationProjectTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectTest.java
@@ -26,12 +26,13 @@ public class MultiConfigurationProjectTest extends BaseTest {
     }
 
     @Test
-    public void testAddDescription() {
-        createItem("MCP", "hudson_matrix_MatrixProject");
+    public void testAddDescription() throws InterruptedException {
+        createItem("MCProject", "hudson_matrix_MatrixProject");
         final String text = "❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F❤\uFE0F";
         returnToDashBoard();
 
-        getDriver().findElement(By.cssSelector("[href = 'job/MCP/']")).click();
+        getDriver().findElement(By.cssSelector("[href = 'job/MCProject/']")).click();
+        Thread.sleep(5000);
         getWait15().until(ExpectedConditions.visibilityOfElementLocated(By.id("description-link"))).click();
         getDriver().findElement(By.name("description")).sendKeys(text);
         getDriver().findElement(By.name("Submit")).click();
