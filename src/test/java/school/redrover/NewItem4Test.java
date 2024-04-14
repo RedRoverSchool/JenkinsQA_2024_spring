ae360cdc851e64c306294b8228cad6a46e3dcc31
Svetlana Dorado <dorado.svetlana@gmail.com>
2024-04-14 20:24:29 +0200

    TC_00.000.11 | Make sure it is impossible to create new item with no name

---
+++ src/test/java/school/redrover/NewItem4Test.java
@@ -0,0 +1,26 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+    public class NewItem4Test extends BaseTest {
+
+        private static final By BUTTON_NEW_ITEM = By.linkText("New Item");
+        private static final By BUTTON_SUBMIT = By.id("ok-button");
+
+        @Test
+        public void testCreateItemWithNoName() {
+            getDriver().findElement(BUTTON_NEW_ITEM).click();
+
+            getDriver().findElement(BUTTON_SUBMIT).click();
+            WebElement inputValidationMassage = getDriver().findElement(By.id("itemname-required"));
+
+            Assert.assertEquals(inputValidationMassage.getText(),
+                    "» This field cannot be empty, please enter a valid name");
+        }
+    }
+
+
