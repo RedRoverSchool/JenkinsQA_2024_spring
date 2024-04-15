002c2ceecc1f2dad187c5ecb73212d8cdf48811b
Elena Pakhomova <pakhomova.e@gmail.com>
2024-04-15 11:15:06 +0300

    TC_00.000.05 deleted blank line

--- src/test/java/school/redrover/NewItem1Test.java
+++ src/test/java/school/redrover/NewItem1Test.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -20,5 +21,4 @@ public class NewItem1Test extends BaseTest {
                 By.cssSelector("div.btn-decorator > button#ok-button"))
                 .isEnabled());
     }
-
 }
