330967b558585fc3ee77ba67db7e9be29572bb57
IanaBlinova <blinovaianaa@gmail.com>
2024-04-15 20:07:32 +0500

    https://trello.com/c/zojdd7tq/115-tc0200703-pipeline-delete-pipeline

---
+++ src/test/java/school/redrover/DeletePipeline2Test.java
@@ -0,0 +1,48 @@
+package school.redrover;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class DeletePipeline2Test extends BaseTest {
+    @Test
+    public void createPipeline() {
+        getDriver().findElement(By.linkText("New Item")).click();
+        WebElement field = getDriver().findElement(By.className("jenkins-input"));
+        field.sendKeys("Pipeline1");
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("description")).sendKeys("Наш новый пайплайн");
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+    }
+
+    @Test
+    public void testCheckPipeline() {
+        createPipeline();
+
+        getDriver().findElement(By.xpath("//a[@href='job/Pipeline1/']")).click();
+        getDriver().findElement(By.xpath("//a[@href='#']")).click();
+
+        Assert.assertEquals("Delete the Pipeline ‘Pipeline1’?", "Delete the Pipeline ‘Pipeline1’?");
+
+        getDriver().findElement(By.xpath("//*[@data-id='cancel']")).click();
+        getDriver().findElement(By.linkText("Pipeline1")).isDisplayed();
+    }
+
+    @Test
+    public void testDeletePipeline() {
+        createPipeline();
+
+        getDriver().findElement(By.xpath("//a[@href='job/Pipeline1/']")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[5]/span/a")).click();
+
+        Assert.assertEquals("Delete the Pipeline ‘Pipeline1’?", "Delete the Pipeline ‘Pipeline1’?");
+
+        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+
+    }
+}
+
+
