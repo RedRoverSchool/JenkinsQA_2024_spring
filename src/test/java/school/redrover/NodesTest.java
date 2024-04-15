b38c0d0bbe2c3fa2480e260b036cc9cf67bf70d0
Denis Prokofyev <denisprokofyev@gmail.com>
2024-04-15 13:49:42 +0300

    TC_15.003.01 Test Built-In Node Monitoring Data list

--- src/test/java/school/redrover/NodesTest.java
+++ src/test/java/school/redrover/NodesTest.java
@@ -1,9 +1,13 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NodesTest extends BaseTest {
 
@@ -17,5 +21,25 @@ public class NodesTest extends BaseTest {
         text = getDriver().findElement(By.xpath("//h1")).getText();
 
         Assert.assertEquals(text,"New node");
+    }
+
+    @Test
+    public void testBuiltInNodeMonitoringDataList() {
+        final List<String> expectedMonitoringDataValues = List.of("Architecture", "Response Time", "Clock Difference",
+                "Free Temp Space", "Free Disk Space", "Free Swap Space");
+
+        getDriver().findElement(By.cssSelector("[href='/computer/']")).click();
+        getDriver().findElement(By.cssSelector("[href*='built-in']")).click();
+        getDriver().findElement(By.className("advancedButton")).click();
+
+        List<WebElement> monitoringDataElements = getDriver()
+                .findElements(By.cssSelector("[class*='dropdown'] td:nth-of-type(odd)"));
+        List<String> actualMonitoringDataValues = new ArrayList<>();
+        for (WebElement element : monitoringDataElements) {
+            actualMonitoringDataValues.add(element.getText());
+        }
+
+        Assert.assertEquals(actualMonitoringDataValues, expectedMonitoringDataValues,
+                "Actual Monitoring Data list is different");
     }
 }
