0b359be60fb42e1ade22e7ae3a232294c9bfd2ca
irina <irinafrutiz@gmail.com>
2024-04-15 17:52:42 +0200

    TC_00.001.07 class's variables move to test an d delete comment

--- src/test/java/school/redrover/FreestyleProject5Test.java
+++ src/test/java/school/redrover/FreestyleProject5Test.java
@@ -7,12 +7,11 @@ import school.redrover.runner.BaseTest;
 
 public class FreestyleProject5Test extends BaseTest {
 
-    private static final String PROJECT_NAME = "ProjectName";
-
-    private static final By PROJECT_LINK = By.xpath("//*[@href='job/" + PROJECT_NAME + "/']/span");
-
     @Test
     public void testCheckFreestyleProjectNameOnTheDashboard() {
+        String PROJECT_NAME = "ProjectName";
+        By PROJECT_LINK = By.xpath("//*[@href='job/" + PROJECT_NAME + "/']/span");
+
         getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
@@ -22,10 +21,8 @@ public class FreestyleProject5Test extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(PROJECT_LINK).getText(), PROJECT_NAME);
 
-//        post condition
         getDriver().findElement(PROJECT_LINK).click();
         getDriver().findElement(By.xpath("//*[@data-url='/job/" + PROJECT_NAME + "/doDelete']")).click();
         getDriver().findElement(By.xpath("//*[@data-id='ok']")).click();
-
     }
 }
