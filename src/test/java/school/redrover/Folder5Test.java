8a88d1cb8f12445d16e995c539a10432d6f382c8
elenadl <ederyabina@aol.com>
2024-04-15 12:30:19 -0700

    ed/Edit2 Folder5Test

--- src/test/java/school/redrover/Folder5Test.java
+++ src/test/java/school/redrover/Folder5Test.java
@@ -12,23 +12,30 @@ import java.util.List;
 public class Folder5Test extends BaseTest {
     public void createFolder(String folderName) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+
         getDriver().findElement(By.id("name")).sendKeys(folderName);
+
         getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']")).click();
+
         getDriver().findElement(By.id("ok-button")).click();
+
         getDriver().findElement(By.id("jenkins-home-link")).click();
     }
+
     @Test
     public void deleteFolderTest() {
-        String folderName = "DeleteFolder";
+        final String folderName = "DeleteFolder";
 
         createFolder(folderName);
 
         getDriver().findElement(By.xpath("//table//a[@href='job/" + folderName + "/']")).click();
+
         getDriver().findElement(By.xpath("//a[@data-title='Delete Folder']")).click();
+
         getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
 
-        List<WebElement> jobList = getDriver()
-                .findElements(By.xpath("//table//a[@href='job/" + folderName +"/']"));
+        List<WebElement> jobList = getDriver().findElements(
+                By.xpath("//table//a[@href='job/" + folderName +"/']"));
 
         Assert.assertTrue(jobList.isEmpty());
     }
