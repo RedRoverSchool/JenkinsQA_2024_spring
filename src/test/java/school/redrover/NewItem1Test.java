d92b82949e30dc439c09c815df899fd31c167965
Elena Pakhomova <pakhomova.e@gmail.com>
2024-04-15 13:31:34 +0300

    TC_00.000.04 add test

--- src/test/java/school/redrover/NewItem1Test.java
+++ src/test/java/school/redrover/NewItem1Test.java
@@ -2,9 +2,14 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
 
 public class NewItem1Test extends BaseTest {
 
@@ -21,4 +26,27 @@ public class NewItem1Test extends BaseTest {
                 By.cssSelector("div.btn-decorator > button#ok-button"))
                 .isEnabled());
     }
+    @Test
+    public void testNewItemUnsafeCharInName() {
+        String unsafeSymbol = "$";
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofMillis(1000));
+
+        getDriver().findElement(
+                By.xpath("//a[@href='newJob']"))
+                .click();
+        getDriver().findElement(
+                By.cssSelector("div.add-item-name > input#name"))
+                .sendKeys(unsafeSymbol);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div#itemname-invalid")));
+
+        WebElement validationMessage = getDriver().findElement(
+                By.cssSelector("div#itemname-invalid"));
+
+        Assert.assertEquals((validationMessage).getText(),
+                String.format("» ‘%s’ is an unsafe character", unsafeSymbol));
+        assert (Color.fromString((validationMessage).getCssValue("color"))
+                .asHex().equals("#ff0000"));
+    }
+
 }
