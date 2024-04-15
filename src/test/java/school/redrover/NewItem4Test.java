6b574c8ca83ec713c253c2ebedf0b8f21558cd9e
miskarov <miskarov.dk@gmail.com>
2024-04-15 00:24:37 -0500

    TC_00.000.15 | Added redirect test to new item page from Welcome to Jenkins block

--- src/test/java/school/redrover/NewItem4Test.java
+++ src/test/java/school/redrover/NewItem4Test.java
@@ -10,9 +10,16 @@ public class NewItem4Test extends BaseTest {
     final String newItemButton = "//a[@href='/view/all/newJob']";
 
     @Test
-    public void testEnterNewItemPageFromLeftPanel() {
+    public void testEnterNewItemPageFromLeftPanel(){
         getDriver().findElement(By.xpath(newItemButton)).click();
 
         Assert.assertEquals(getDriver().getCurrentUrl(), newItemURL);
+    }
+
+    @Test
+    public void testEnterNewItemPageFromWelcomeBlock(){
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/newJob");
     }
 }
