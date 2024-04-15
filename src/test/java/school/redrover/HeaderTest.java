8c3ec1854ca5a8f68910cb178cd7cd338748aac5
Turii <o.turij@gmail.com>
2024-04-15 09:53:05 -0700

    TC_14.002.01 | Header > Seach box > Search functionality > Verify Search result is displayed

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -3,15 +3,11 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
-
 public class HeaderTest extends BaseTest {
-    WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
 
     @Test
     public void testLogoJenkins() {
