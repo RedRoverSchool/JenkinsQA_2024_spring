65922131e8638adf14af11033ccbc8c008d5b224
Kuba Bekoff <k.bekoff.5@gmail.com>
2024-04-17 20:22:21 +0600

    TC_01.002.05 | add method to the test

--- src/test/java/school/redrover/ProjectCreateTest.java
+++ src/test/java/school/redrover/ProjectCreateTest.java
@@ -1,6 +1,6 @@
 package school.redrover;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -19,7 +19,24 @@ public class ProjectCreateTest extends BaseTest {
         getDriver().findElement(By.name("Submit")).click();
         text = getDriver().findElement(By.xpath("//h1")).getText();
 
-        Assert.assertEquals(text,"Test project");
+        Assert.assertEquals(text, "Test project");
 
     }
+    private final WebDriver driver;
+
+    public ProjectCreateTest(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void projectActions(String projectName) {
+        projectName = "Freestyle project";
+        driver.findElement(By.xpath(
+                "//*[@href='/view/all/newJob']")).click();
+        driver.findElement(By.id("name")).sendKeys(projectName);
+        driver.findElement(By.xpath(
+                "//*[@class='hudson_model_FreeStyleProject']")).click();
+        driver.findElement(By.id("ok-button")).click();
+        driver.findElement(By.name("Submit")).click();
+    }
+
 }
