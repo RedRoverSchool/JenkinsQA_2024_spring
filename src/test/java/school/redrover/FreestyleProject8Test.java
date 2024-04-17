699933d2b41713fa4289fd523f0872b76efd6264
Yakov Tereshenkov <tereshenkov@gmail.com>
2024-04-17 18:34:49 +0300

    TC_01.002.05 fix CI error

--- src/test/java/school/redrover/FreestyleProject8Test.java
+++ src/test/java/school/redrover/FreestyleProject8Test.java
@@ -7,12 +7,38 @@ import school.redrover.runner.BaseTest;
 
 public class FreestyleProject8Test extends BaseTest {
 
+    public void createFreestyleProject(String projectName) {
+        String projectNameLocal = "Freestyle project";
+        getDriver().findElement(By.xpath(
+                "//*[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectNameLocal);
+        getDriver().findElement(By.xpath(
+                "//*[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+    }
+
+    @Test
+    public void testCreateFreestyleProject() {
+        String text;
+
+        getDriver().findElement(By.xpath(
+                "//*[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Test project");
+        getDriver().findElement(By.xpath(
+                "//*[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        text = getDriver().findElement(By.xpath("//h1")).getText();
+
+        Assert.assertEquals(text, "Test project");
+    }
+
     @Test
     public void testRenamingFreestyleProject() {
         String text;
-        ProjectCreateTest createProject = new ProjectCreateTest(getDriver());
 
-        createProject.projectActions("Freestyle project");
+        createFreestyleProject("Freestyle project");
         getDriver().findElement(By.xpath("//*[contains(@href, 'rename')]")).click();
         getDriver().findElement(By.xpath("//*[@name='newName']")).clear();
         getDriver().findElement(By.xpath(
