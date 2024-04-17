1c4392956ee33d2ec5723d08404ef8b1ec638bd3
Yaroslav-R-P <mirexov@mail.ru>
2024-04-17 08:37:51 +0300

    RF _Refactoring NewItem8Test Class

--- src/test/java/school/redrover/NewItem8Test.java
+++ src/test/java/school/redrover/NewItem8Test.java
@@ -9,13 +9,13 @@ public class NewItem8Test extends BaseTest {
 
     @Test
     public void testCreateItem() {
-
         final String itemName = "My first Multibranch Pipeline";
 
         getDriver().findElement(By.className("task")).click();
         getDriver().findElement(By.className("jenkins-input")).sendKeys(itemName);
         getDriver().findElement(By.xpath("(//li[@tabindex='0'])[5]")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+
         Assert.assertTrue(getDriver().findElement(By.linkText(itemName)).isDisplayed());
     }
 }
