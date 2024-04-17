4397885169bbba2faf762b0c311a5919e06b04db
Yakov Tereshenkov <tereshenkov@gmail.com>
2024-04-17 21:40:07 +0300

    TC_01.002.05

--- src/test/java/school/redrover/FreestyleProject8Test.java
+++ src/test/java/school/redrover/FreestyleProject8Test.java
@@ -7,28 +7,34 @@ import school.redrover.runner.BaseTest;
 
 public class FreestyleProject8Test extends BaseTest {
 
-        public void createFreestyleProject() {
+    public void createFreestyleProject(){
+        getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Freestyle project");
+        getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+    }
 
-            getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
-            getDriver().findElement(By.id("name")).sendKeys("Freestyle project");
-            getDriver().findElement(By.xpath(
-                    "//*[@class='hudson_model_FreeStyleProject']")).click();
-            getDriver().findElement(By.id("ok-button")).click();
-            getDriver().findElement(By.name("Submit")).click();
-        }
     @Test
-    public void testRenamingFreestyleProject() {
-        String text;
+    public void testCreateFreestyleProject(){
+        getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Freestyle project");
+        getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
 
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Freestyle project");
+    }
+
+    @Test
+    public void testRenamingFreestyleProject(){
         createFreestyleProject();
         getDriver().findElement(By.xpath("//*[contains(@href, 'rename')]")).click();
         getDriver().findElement(By.xpath("//*[@name='newName']")).clear();
-        getDriver().findElement(By.xpath(
-                "//*[@name='newName']")).sendKeys("New name");
+        getDriver().findElement(By.xpath("//*[@name='newName']")).sendKeys("New name");
         getDriver().findElement(By.xpath("//*[@name = 'Submit']")).click();
-        text = getDriver().findElement(By.xpath("//h1")).getText();
 
-        Assert.assertEquals(text, "New name");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "New name");
     }
 
 }
