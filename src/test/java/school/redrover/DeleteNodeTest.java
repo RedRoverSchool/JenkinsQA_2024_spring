c7f9acf2afa3a91309fe3a3765bfbe0a0879ceda
MeerimB <meerimb312@gmail.com>
2024-04-18 11:42:36 +0300

    TC_15.002.01 | Manage Jenkins > Nodes > Delete Node

--- src/test/java/school/redrover/DeleteNodeTest.java
+++ src/test/java/school/redrover/DeleteNodeTest.java
@@ -1,6 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -9,10 +11,12 @@ public class DeleteNodeTest extends BaseTest {
 
     public void testCreateNode() {
 
+        final String nodName = "TestNode";
+
         getDriver().findElement(By.linkText("Manage Jenkins")).click();
         getDriver().findElement(By.xpath("//a[@href='computer']")).click();
         getDriver().findElement(By.xpath("//a[@href='new']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("TestNode");
+        getDriver().findElement(By.id("name")).sendKeys(nodName);
         getDriver().findElement(By.xpath("//label[@class='jenkins-radio__label']")).click();
         getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate' and @name='Submit']")).click();
@@ -21,16 +25,18 @@ public class DeleteNodeTest extends BaseTest {
     @Test
     public void testDeleteExistingNode() {
 
-        final String expectedResult = "Nodes";
+        final String searchNode = "TestNode";
 
         testCreateNode();
 
-        getDriver().findElement(By.xpath("//a[@href='../computer/TestNode/']")).click();
-        getDriver().findElement(By.xpath("//span[text()='Delete Agent']")).click();
+        getDriver().findElement(By.linkText(searchNode)).click();
+        getDriver().findElement(By.xpath("//a[.='Delete Agent']")).click();
         getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
 
-        final String actualResult = getDriver().findElement(By.xpath("//div[@class='jenkins-app-bar']//h1")).getText();
+        WebElement searchBox = getDriver().findElement(By.id("search-box"));
+        searchBox.sendKeys(searchNode);
+        searchBox.sendKeys(Keys.ENTER);
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class='error']")).getText(), "Nothing seems to match.");
     }
 }
