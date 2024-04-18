949a25e2fcc3f6e77d18028f6eecc61e9b4639ab
iKuper <amalgammy@mail.ru>
2024-04-18 14:17:10 +0300

    TС_09.005.01 | Manage Jenkins > Appearance > Verify quantity of Themes

--- src/test/java/school/redrover/ManageAppearanceTest.java
+++ src/test/java/school/redrover/ManageAppearanceTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -22,10 +23,9 @@ public class ManageAppearanceTest extends BaseTest {
 //                .moveToElement(dashboard.findElement(By.cssSelector("[class$='chevron']")))
                 .scrollToElement(dashboard.findElement(By.cssSelector("[class$='chevron']")))
                 .click(dashboard.findElement(By.cssSelector("[class$='chevron']")))
-                .pause(1500)
                 .perform();
 
-          getDriver().findElement(By.cssSelector("[class='tippy-box'] [href='/manage']")).click();
+          getWait15(this).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='tippy-box'] [href='/manage']"))).click();
           getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
 
 //          sleep(this, 3);
