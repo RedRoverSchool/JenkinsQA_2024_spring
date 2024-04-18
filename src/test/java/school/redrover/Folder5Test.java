de6d1af1a6e72f53bec2b40add8333ef2114429a
Natalia <natalia.mylova@gmail.com>
2024-04-18 18:22:53 +0100

    TC_04.003.01/add new test to Folder5Test

--- src/test/java/school/redrover/Folder5Test.java
+++ src/test/java/school/redrover/Folder5Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -36,5 +37,24 @@ public class Folder5Test extends BaseTest {
                 By.xpath("//table//a[@href='job/" + folderName +"/']"));
 
         Assert.assertTrue(jobList.isEmpty());
+    }
+
+    @Test
+    public void deleteFolderFromDropDownTest() {
+        final String folderName = "DeleteFolderFromDropDown";
+
+        createFolder(folderName);
+
+        WebElement element1 = getDriver().findElement(By.xpath("//span[.='DeleteFolderFromDropDown']"));
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(element1).perform();
+
+        getDriver().findElement(By.xpath("//tr[@id='job_DeleteFolderFromDropDown']//button[@class='jenkins-menu-dropdown-chevron']")).click();
+
+        getDriver().findElement(By.cssSelector("[href='/job/DeleteFolderFromDropDown/doDelete']")).click();
+
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText(), "Welcome to Jenkins!");
     }
 }
