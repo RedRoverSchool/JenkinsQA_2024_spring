cf419cfb942fed31675459009e0f84ee4c700701
erokh <erokhine@mail.ru>
2024-04-18 13:57:25 +0300

    TC_04.001.06 | Folder > Rename Folder

--- src/test/java/school/redrover/runner/Folder7Test.java
+++ src/test/java/school/redrover/runner/Folder7Test.java
@@ -1,28 +1,27 @@
 package school.redrover.runner;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Folder7Test extends BaseTest {
 
     final String OLD_NAME = "Random Folder";
-    final String NEW_FOLDER_NAME = "Renamed Folder";
+    final String NEW_NAME = "Renamed Folder";
 
-    public void createFolderUsingName(String name, WebDriver driver) {
+    public void createFolderUsingName(String name) {
 
-        driver.findElement(By.linkText("New Item")).click();
-        driver.findElement(By.id("name")).sendKeys(name);
-        driver.findElement(By.xpath("//*[@id='j-add-item-type-nested-projects']/ul/li[1]")).click();
-        driver.findElement(By.id("ok-button")).click();
-        driver.findElement(By.name("Submit")).click();
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(name);
+        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-nested-projects']/ul/li[1]")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
     }
 
     @Test
     public void renameFolderNameViaDropdown() {
 
-        createFolderUsingName(OLD_NAME, getDriver());
+        createFolderUsingName(OLD_NAME);
 
         getDriver().findElement(By.id("jenkins-name-icon")).click();
         getDriver().findElement(By.linkText(OLD_NAME)).click();
@@ -30,11 +29,11 @@ public class Folder7Test extends BaseTest {
 
         getDriver().findElement(By.xpath("//*[@class='setting-main']/input")).clear();
         getDriver().findElement(By.xpath("//*[@class='setting-main']/input")).sendKeys(
-                NEW_FOLDER_NAME);
+                NEW_NAME);
         getDriver().findElement(By.name("Submit")).click();
 
         getDriver().findElement(By.id("jenkins-name-icon")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.linkText(NEW_FOLDER_NAME)).getText(), "Renamed Folder");
+        Assert.assertEquals(getDriver().findElement(By.linkText(NEW_NAME)).getText(), "Renamed Folder");
     }
 }
