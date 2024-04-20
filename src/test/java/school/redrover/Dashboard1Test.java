83e462e8c60b1c7b8ad2d6a23ab0f40a8b07aab5
Serhii Malko <malsv@ukr.net>
2024-04-20 19:13:35 +0300

    add testCreateViewWithSelectedItems

--- src/test/java/school/redrover/Dashboard1Test.java
+++ src/test/java/school/redrover/Dashboard1Test.java
@@ -1,8 +1,10 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
@@ -11,52 +13,81 @@ import java.util.*;
 
 public class Dashboard1Test extends BaseTest {
 
-    public List<String> getNamesList() {
+    private final String SELECTED_NAME1 = "Vivaldi";
+    private final String SELECTED_NAME2 = "Nic";
+
+    private List<String> getNamesList() {
         List<String> names = new ArrayList<>();
         names.add("Imagine Dragons");
-        names.add("Queen");
         names.add("A-ha");
         names.add("Depeche Mode");
-        names.add("Rammstein");
         names.add("Antonio Vivaldi");
+        names.add("Niccolo Paganini");
         return names;
     }
 
-    public void createItemsFromList(List<String> list) {
+    private void createItemsFromList(List<String> list) {
         for (String name : list) {
             TestUtils.createItem(TestUtils.MULTIBRANCH_PIPELINE, name, this);
             TestUtils.goToMainPage(getDriver());
         }
     }
 
-    public List<String> getItemNamesFromColumnAfterSortingByName() {
+    private List<String> getItemNamesFromColumnAfterSortingByName() {
         getDriver().findElement(By.xpath("//a[@class='sortheader' and text()='Name']")).click();
         return TestUtils.getTexts(getDriver().findElements(By.xpath("//td/a[contains(@href, 'job/')]")));
     }
 
+    private void clickElement(WebElement webElement) {
+        new Actions(getDriver())
+                .scrollToElement(webElement)
+                .scrollByAmount(0, 100)
+                .moveToElement(webElement)
+                .click()
+                .perform();
+    }
+
     @Test
-    public void testSortItems() {
+    public void testSortItemsByName() {
         List<String> names = getNamesList();
         createItemsFromList(names);
         Collections.sort(names);
         Collections.reverse(names);
-
-        Assert.assertEquals(getItemNamesFromColumnAfterSortingByName(), names);
+        boolean isDescendingSortingOk = getItemNamesFromColumnAfterSortingByName().equals(names);
 
         Collections.reverse(names);
+        boolean isAscendingSortingOk = getItemNamesFromColumnAfterSortingByName().equals(names);
 
-        Assert.assertEquals(getItemNamesFromColumnAfterSortingByName(), names);
+        Assert.assertTrue(isAscendingSortingOk && isDescendingSortingOk);
     }
 
-//    @Ignore
-//    @Test
-//    public void testCreateViewWithSelectedItem() {
-//        createItemsFromList(getNamesList());
-//
-//        getDriver().findElement(By.xpath("//a[@title='New View']")).click();
-//        getDriver().findElement(By.id("Name")).sendKeys("Classic");
-//        getDriver().findElement(By.id("hudson.model.ListView")).click();
-//        getDriver().findElement(By.id("ok")).click();
-//
-//    }
+    @Test(dependsOnMethods = "testSortItemsByName")
+    public void testCreateViewWithSelectedItems() {
+
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='New View']"))).click();
+        getDriver().findElement(By.id("name")).sendKeys("Classic");
+        getDriver().findElement(By.xpath("//label[@for='hudson.model.ListView']")).click();
+        getDriver().findElement(By.id("ok")).click();
+
+        WebElement selectedProject1 = getDriver().findElement(
+                By.xpath("//label[contains(@title, '" + SELECTED_NAME1 + "')]"));
+
+        WebElement selectedProject2 = getDriver().findElement(
+                By.xpath("//label[contains(@title, '" + SELECTED_NAME2 + "')]"));
+
+        WebElement okButton = getDriver().findElement(
+                By.name("Submit"));
+
+        clickElement(selectedProject1);
+        clickElement(selectedProject2);
+        clickElement(okButton);
+
+        List<String> namesFromNewView = TestUtils.getTexts
+                (getDriver().findElements(By.xpath("//td/a[contains(@href, 'job/')]")));
+        boolean isName1InView = namesFromNewView.stream().anyMatch(s -> s.contains(SELECTED_NAME1));
+        boolean isName2InView = namesFromNewView.stream().anyMatch(s -> s.contains(SELECTED_NAME2));
+
+        Assert.assertTrue(namesFromNewView.size() == 2 && isName1InView && isName2InView);
+    }
+
 }
