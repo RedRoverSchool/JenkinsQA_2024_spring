f0de0ed504c74fed40d5077fb2787e7b75de288e
Denis Prokofyev <denisprokofyev@gmail.com>
2024-04-20 15:48:39 +0300

    TC_02.005.06 Verify text area border RGBA backlight being active

---
+++ src/test/java/school/redrover/PipelineTest.java
@@ -0,0 +1,40 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
+
+public class PipelineTest extends BaseTest {
+
+    private static final String PIPELINE_NAME = "FirstPipeline";
+    private static final By ADD_DESCRIPTION_LOCATOR = By.id("description-link");
+
+    private void createPipelineWithCreateAJob() {
+        getDriver().findElement(By.linkText("Create a job")).click();
+        getDriver().findElement(By.id("name")).sendKeys(PIPELINE_NAME);
+        getDriver().findElement(By.cssSelector("[class$='WorkflowJob']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+    }
+
+    @Test
+    public void testPipelineDescriptionTextAreaBacklightColor() {
+        createPipelineWithCreateAJob();
+        getDriver().findElement(ADD_DESCRIPTION_LOCATOR).click();
+
+        getWait2().until(ExpectedConditions.invisibilityOfElementLocated(ADD_DESCRIPTION_LOCATOR));
+        String currentTextAreaBorderBacklightColor = getDriver().switchTo().activeElement().
+                getCssValue("box-shadow").split(" 0px")[0];
+
+        Assert.assertEquals(currentTextAreaBorderBacklightColor, "rgba(11, 106, 162, 0.25)",
+                "Text area RGBA is not equal to " + currentTextAreaBorderBacklightColor);
+    }
+}
