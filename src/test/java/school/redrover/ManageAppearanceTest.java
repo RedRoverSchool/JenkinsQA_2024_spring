6b59cf8a40356602e63084ef87e248eccaa10599
iKuper <amalgammy@mail.ru>
2024-04-20 19:19:43 +0300

    TС_09.005.01 | Manage Jenkins > Appearance > Verify quantity of Themes

--- src/test/java/school/redrover/ManageAppearanceTest.java
+++ src/test/java/school/redrover/ManageAppearanceTest.java
@@ -19,22 +19,23 @@ public class ManageAppearanceTest extends BaseTest{
             WebElement dashboard = getDriver().findElement(By.linkText("Dashboard"));
 
             new Actions(getDriver())
-                    .moveToElement(dashboard)
-                    .pause(1000)
-                    .moveToLocation(98, 72)
-                    .pause(1000)
-                    .click()
+//                    .moveToElement(dashboard)
+//                    .pause(1000)
+//                    .moveToLocation(98, 72)
+//                    .pause(1000)
+//                    .click()
 //                    .moveToElement(dashboard.findElement(By.cssSelector("[class$='chevron']")))
-//                    .scrollToElement(dashboard.findElement(By.cssSelector("[class$='chevron']")))
-//                    .click(dashboard.findElement(By.cssSelector("[class$='chevron']")))
+                    .scrollToElement(dashboard.findElement(By.cssSelector("[class$='chevron']")))
+                    .pause(1000)
+                    .click(dashboard.findElement(By.cssSelector("[class$='chevron']")))
                     .perform();
 
-            java.awt.Point loc = MouseInfo.getPointerInfo().getLocation();
+//            java.awt.Point loc = MouseInfo.getPointerInfo().getLocation();
 
-            Point g = dashboard.findElement(By.cssSelector("[class$='chevron']")).getLocation();
-            System.out.println(g);
+//            Point g = dashboard.findElement(By.cssSelector("[class$='chevron']")).getLocation();
+//            System.out.println(g);
 
-            TestUtils.sleep(this, 2);
+//            TestUtils.sleep(this, 2);
             getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='tippy-box'] [href='/manage']"))).click();
             getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
 
