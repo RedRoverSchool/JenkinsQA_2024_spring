bac43e58dc2c6858b79e93a9c564289a20f43899
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-04-21 13:25:31 +0300

    TC_00.004.15 | New item > Create Folder > Check if folder can be created

--- src/test/java/school/redrover/Folder4Test.java
+++ src/test/java/school/redrover/Folder4Test.java
@@ -1,13 +1,28 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
 
-import static school.redrover.runner.TestUtils.*;
+import java.lang.reflect.Method;
+import java.time.Duration;
 
 public class Folder4Test extends BaseTest {
+    private final String projectName = "This is the folder name";
+
+    @BeforeMethod
+    @Override
+    protected void beforeMethod(Method method) {
+        super.beforeMethod(method);
+        getDriver().manage().timeouts().implicitlyWait(Duration.ZERO);
+    }
 
     @Test
     public void testCreateNewFolder() {
@@ -16,8 +31,23 @@ public class Folder4Test extends BaseTest {
         getDriver().findElement(By.cssSelector("[class *= 'plugins_folder_Folder']")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
-        returnToDashBoard(this);
+        TestUtils.returnToDashBoard(this);
 
         Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='NewFolder']")).isDisplayed());
+    }
+
+    @Test
+    public void testCreateFolder() {
+        WebElement dashboardElement =  getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#breadcrumbBar a[href = '/']")));
+
+        TestUtils.openElementDropdown(this, dashboardElement);
+
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.tippy-box a[href $= 'newJob']"))).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("name"))).sendKeys(projectName);
+        getDriver().findElement(By.className(TestUtils.Item.FOLDER)).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        Assert.assertTrue(
+                getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.linkText(projectName))).isDisplayed());
     }
 }
