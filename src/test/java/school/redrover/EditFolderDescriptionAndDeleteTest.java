395d612ba45b812d5bca54bb3a85c116149020d0
IanaBlinova <blinovaianaa@gmail.com>
2024-04-21 17:08:23 +0500

    https://trello.com/c/G2cbqpia/331-tc0400403-folder-add-and-edit-description

--- src/test/java/school/redrover/EditFolderDescriptionAndDeleteTest.java
+++ src/test/java/school/redrover/EditFolderDescriptionAndDeleteTest.java
@@ -8,28 +8,30 @@ import school.redrover.runner.BaseTest;
 
 import java.util.List;
 
-public class EditFolderDescriptionAndDeleteTest extends BaseTest{
-    private final String NAME_FOLDER = "Folder1";
+public class EditFolderDescriptionAndDeleteTest extends BaseTest {
+    private final String NAME_FOLDER = "FolderFolder";
     private final String DESCRIPTION_TEXT = "Добавили описание";
     private final String DESCRIPTION_LOCATOR = "description";
     private final String DESCRIPTION_BUTTON = "description-link";
     private final String BUTTON_SAVE = "button[name='Submit']";
+
     @Test
     public void testAddFolder() {
-       getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-       WebElement nameFolder = getDriver().findElement(By.id("name"));
-       nameFolder.sendKeys(NAME_FOLDER);
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        WebElement nameFolder = getDriver().findElement(By.id("name"));
+        nameFolder.sendKeys(NAME_FOLDER);
 
-       getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
-       getDriver().findElement(By.id("ok-button")).click();
-       getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.cssSelector(BUTTON_SAVE)).click();
 
-       String checkNameFolder = getDriver().findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/h1[1]")).getText();
+        String checkNameFolder = getDriver().findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/h1[1]")).getText();
 
-       Assert.assertEquals(checkNameFolder, NAME_FOLDER);
+        Assert.assertEquals(checkNameFolder, NAME_FOLDER);
 
-       getDriver().findElement(By.id("jenkins-name-icon")).click();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
     }
+
     @Test
     public void testAddDescription() {
         testAddFolder();
@@ -46,6 +48,7 @@ public class EditFolderDescriptionAndDeleteTest extends BaseTest{
         getDriver().findElement(By.cssSelector(BUTTON_SAVE)).click();
 
     }
+
     @Test
     public void testEditDescription() {
         testAddDescription();
@@ -57,6 +60,7 @@ public class EditFolderDescriptionAndDeleteTest extends BaseTest{
         getDriver().findElement(By.cssSelector(BUTTON_SAVE)).click();
 
     }
+
     @Test
     public void testDeleteDescription() {
         testEditDescription();
@@ -64,4 +68,18 @@ public class EditFolderDescriptionAndDeleteTest extends BaseTest{
         getDriver().findElement(By.name(DESCRIPTION_LOCATOR)).clear();
         getDriver().findElement(By.cssSelector(BUTTON_SAVE)).click();
     }
+
+    @Test
+    public void testDeleteFolder() {
+        testAddFolder();
+        getDriver().findElement(
+                By.xpath("//table//a[@href='job/" + NAME_FOLDER + "/']")).click();
+        getDriver().findElement(By.xpath("//a[@data-title='Delete Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+        List<WebElement> jobList = getDriver().findElements(
+                By.xpath("//table//a[@href='job/" + NAME_FOLDER +"/']"));
+
+        Assert.assertTrue(jobList.isEmpty());
+    }
+
 }
