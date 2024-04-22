929d869ec282815a12dd1a9ee1b6471582783c5a
iKuper <amalgammy@mail.ru>
2024-04-22 12:50:50 +0300

    final

--- src/test/java/school/redrover/ManageAppearanceTest.java
+++ src/test/java/school/redrover/ManageAppearanceTest.java
@@ -2,14 +2,11 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.TestUtils;
 
 public class ManageAppearanceTest extends BaseTest{
 
