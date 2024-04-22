42945f899f24c589ca2cba4967bfdc4b715b0124
MariJern <marina.jtest@gmail.com>
2024-04-21 23:05:52 -0500

    TC_01.002.01| Freestyle Project > Rename Project > Rename project from Dropdown menu

--- src/test/java/school/redrover/FreestyleProject3Test.java
+++ src/test/java/school/redrover/FreestyleProject3Test.java
@@ -2,15 +2,22 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
 
 import java.util.List;
 
 public class FreestyleProject3Test extends BaseTest {
     private final static String FREESTYLE_PROJECT_NAME = "new Freestyle project";
+    private final static String RENAMED_PROJECT_NAME = "old Freestyle project";
+
+    public void clickJenkinsHomePage() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+    }
 
     private void createFreestyleProject(String projectName) {
         getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
@@ -67,6 +74,27 @@ public class FreestyleProject3Test extends BaseTest {
                 By.xpath("//span[text() = '" + FREESTYLE_PROJECT_NAME + "']"));
 
         Assert.assertTrue(projectList.isEmpty());
+    }
+
+    @Test
+    public void renameFreestyleProjectFromDropdown() {
+
+        createFreestyleProject(FREESTYLE_PROJECT_NAME);
+        clickJenkinsHomePage();
+
+        WebElement dropdownChevron  = getDriver().findElement(By.xpath("//td[3]"));
+        TestUtils.openElementDropdown(this, dropdownChevron);
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Rename"))).click();
+
+        WebElement projectNameInputField = getDriver().findElement(By.xpath("//input[@class='jenkins-input validated  ']"));
+        projectNameInputField.clear();
+        projectNameInputField.sendKeys(RENAMED_PROJECT_NAME);
+
+        getDriver().findElement(By.name("Submit")).click();
+
+        String ActualProjectName = getDriver().findElement(By.tagName("h1")).getText();
+
+        Assert.assertEquals(ActualProjectName, RENAMED_PROJECT_NAME);
     }
 }
 
