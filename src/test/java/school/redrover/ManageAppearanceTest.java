adf88b6beb9937e40f37110606395b3042a61ec6
iKuper <amalgammy@mail.ru>
2024-04-22 15:06:22 +0300

    TC_09.005.02 | Manage Jenkins > Appearance > Dark theme switch notification check

--- src/test/java/school/redrover/ManageAppearanceTest.java
+++ src/test/java/school/redrover/ManageAppearanceTest.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
 
 public class ManageAppearanceTest extends BaseTest{
 
@@ -22,5 +26,17 @@ public class ManageAppearanceTest extends BaseTest{
             getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
 
             Assert.assertEquals(getDriver().findElements(By.className("app-theme-picker__item")).size(), 3);
+        }
+
+    @Test
+        public void testDarkThemeSwitchNotification() {
+            getDriver().findElement(By.linkText("Manage Jenkins")).click();
+            getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
+            getDriver().findElement(By.cssSelector("[for='radio-block-0']")).click();
+            getDriver().findElement(By.name("Apply")).click();
+
+            Assert.assertEquals(
+                    getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("notification-bar"))).getText(),
+                    "Saved");
         }
     }
