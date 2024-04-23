6877f3f8f367a3c0fa92efe9cc9e48702515a21a
Marina J <marina.jtest@gmail.com>
2024-04-23 09:33:52 -0500

    add static import

--- src/test/java/school/redrover/FreestyleProject3Test.java
+++ src/test/java/school/redrover/FreestyleProject3Test.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
