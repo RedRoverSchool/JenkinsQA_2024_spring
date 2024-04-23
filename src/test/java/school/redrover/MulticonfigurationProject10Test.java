47070c4fa18d4615beb4a809a19699eeaa9544c9
Kuba Bekoff <k.bekoff.5@gmail.com>
2024-04-24 00:22:46 +0600

    renamed class

--- src/test/java/school/redrover/MulticonfigurationProject10Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject10Test.java
@@ -5,13 +5,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class MulticonfigurationProjectEmptyTest extends BaseTest {
+public class MulticonfigurationProject10Test extends BaseTest {
     @Test
     public void testDescriptionField(){
         getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("New project with empty description");
+        getDriver().findElement(By.id("name")).sendKeys("New MultiProject");
         getDriver().findElement(By.xpath("//*[contains(@class,'MatrixProject')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("description")).sendKeys("test");
         getDriver().findElement(By.name("Submit")).click();
 
         Assert.assertTrue(getDriver().findElement(By.xpath
