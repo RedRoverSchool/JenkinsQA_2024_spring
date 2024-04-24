ea8710b8d5ac8d718cbab39d49654ba148e946f2
janalifanova <janalifanova@gmail.com>
2024-04-24 15:42:47 -0500

    Added waiters

--- src/test/java/school/redrover/FreestyleProject13Test.java
+++ src/test/java/school/redrover/FreestyleProject13Test.java
@@ -1,21 +1,26 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+
 public class FreestyleProject13Test extends BaseTest {
 
-    @Ignore
+
     @Test
-    private void testNewFreestyleProjectCreated(){
+    private void testNewFreestyleProjectCreated()  {
         getDriver().findElement(By.linkText("New Item")).click();
+
         getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("Freestyle1");
         getDriver().findElement(By.xpath("//label/span[text() ='Freestyle project']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
+        WebElement okButton = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("ok-button")));
+        okButton.click();
+        WebElement saveButton = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("Submit")));
+        saveButton.click();
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1[contains(@class, 'job-index-headline page-headline')]")).getText(),
                 "Freestyle1");
 
