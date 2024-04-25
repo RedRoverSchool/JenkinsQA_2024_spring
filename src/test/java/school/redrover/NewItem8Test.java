b209c05768ed57bc73192e9fdaf25c9617c1856e
Yaroslav-R-P <mirexov@mail.ru>
2024-04-25 09:47:16 +0300

    TC_00.005.11 New Item  Create Multibranch Pipeline> New Job

--- src/test/java/school/redrover/NewItem8Test.java
+++ src/test/java/school/redrover/NewItem8Test.java
@@ -7,11 +7,21 @@ import school.redrover.runner.BaseTest;
 
 public class NewItem8Test extends BaseTest {
 
+    private final String itemName = "My first Multibranch Pipeline";
+
     @Test
     public void testCreateItem() {
-        final String itemName = "My first Multibranch Pipeline";
-
         getDriver().findElement(By.className("task")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(itemName);
+        getDriver().findElement(By.xpath("(//li[@tabindex='0'])[5]")).click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.linkText(itemName)).isDisplayed());
+    }
+
+    @Test
+    public void testCreateItemFromNewJob() {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
         getDriver().findElement(By.className("jenkins-input")).sendKeys(itemName);
         getDriver().findElement(By.xpath("(//li[@tabindex='0'])[5]")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
