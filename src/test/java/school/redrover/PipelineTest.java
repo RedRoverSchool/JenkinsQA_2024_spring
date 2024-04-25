4444f2403ef11928a7f6467431ef0aa8231fd291
Artem RazDva <razdymakha91@mail.ru>
2024-04-25 19:54:29 -0400

    no more @Ignore

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -60,7 +60,6 @@ public class PipelineTest extends BaseTest {
         Assert.assertEquals(defaultTextAreaBorderBacklightColor, "rgba(11,106,162,.25)");
     }
 
-    @Ignore
     @Test
     public void testYesButtonColorDeletingPipelineInSidebar() {
         TestUtils.resetJenkinsTheme(this);
