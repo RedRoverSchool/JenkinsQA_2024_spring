a2ef40c3f1135b618e2e13671bdd73d076c0c7f3
Aram_H <63616812+AramH20@users.noreply.github.com>
2024-04-26 09:19:11 -0700

    Update CreateFolder1Test.java
    add code

--- src/test/java/school/redrover/CreateFolder1Test.java
+++ src/test/java/school/redrover/CreateFolder1Test.java
@@ -51,6 +51,9 @@ public class CreateFolder1Test extends BaseTest {
         createNewFolder(folderName);
         openDashboard();
 
+        WebElement folder = getDriver().findElement(By.xpath("//tr[@id='job_" + folderName + "']/td[3]/a/span"));
+        new Actions(getDriver()).moveToElement(folder).perform();
+        
         WebElement projectElement = getDriver().findElement(By.xpath("//span[text()='" + folderName + "']"));
         new Actions(getDriver()).moveToElement(projectElement).perform();
         
