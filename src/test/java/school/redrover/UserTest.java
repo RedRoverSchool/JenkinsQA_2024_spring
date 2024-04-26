f2baa04ddcb5df7111b0c2c229c4a4414e864b37
maksin <maksin82@gmail.com>
2024-04-26 16:00:29 +0300

    userTest

--- src/test/java/school/redrover/UserTest.java
+++ src/test/java/school/redrover/UserTest.java
@@ -80,4 +80,32 @@ public class UserTest extends BaseTest {
 
         Assert.assertEquals(names, names.stream().sorted().collect(Collectors.toList()));
     }
+    @Test
+    public void testDeleteUsingMenuDropdownChevron() {
+        final String dataTest = "one";
+
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+        getDriver().findElement(By.xpath("//dt[text() = 'Users']")).click();
+        getDriver().findElement(By.xpath("//*[@href='addUser']")).click();
+        getDriver().findElement(By.id("username")).sendKeys(dataTest);
+        getDriver().findElement(By.name("password1")).sendKeys(dataTest);
+        getDriver().findElement(By.name("password2")).sendKeys(dataTest);
+        getDriver().findElement(By.name("fullname")).sendKeys(dataTest);
+        getDriver().findElement(By.name("email")).sendKeys(dataTest + "@gmail.com");
+        getDriver().findElement(By.name("Submit")).click();
+
+
+        getDriver().findElement(
+                By.xpath("//a[(@href='user/" + dataTest + "/')]")).click();
+
+        getDriver().findElement(By.xpath("//button[@href='/user/"+dataTest+"/doDelete']")).click();
+
+        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/asynchPeople/']")).click();
+
+        List<WebElement> elementList = getDriver().findElements(By.xpath("//*[@id='people']//tbody"));
+        List<String> resultList = elementList.stream().map(WebElement::getText).toList();
+
+        Assert.assertFalse(resultList.contains(dataTest));
+    }
 }
