2644cbff4983f2f469baad15cbb8a094299e2f66
Ivan Denisenko <info@sweetscripts.org>
2024-04-27 16:34:41 -0400

    add to ignore EditFolderDescriptionAndDeleteTest

--- src/test/java/school/redrover/EditFolderDescriptionAndDeleteTest.java
+++ src/test/java/school/redrover/EditFolderDescriptionAndDeleteTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 import java.util.List;
-
+@Ignore
 public class EditFolderDescriptionAndDeleteTest extends BaseTest {
     private final String NAME_FOLDER = "FolderFolder";
     private final String DESCRIPTION_TEXT = "Добавили описание";
