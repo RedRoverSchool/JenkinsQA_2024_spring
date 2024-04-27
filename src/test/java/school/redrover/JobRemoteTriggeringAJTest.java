06d8a516377221dc9794be6653f7dea70e773fc9
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2024-04-27 15:08:12 -0400

    Clean code

--- src/test/java/school/redrover/JobRemoteTriggeringAJTest.java
+++ src/test/java/school/redrover/JobRemoteTriggeringAJTest.java
@@ -18,25 +18,22 @@ public class JobRemoteTriggeringAJTest extends BaseTest {
         getDriver().findElement(By.id("jenkins-name-icon")).click();
         getWait5().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("tasks")));
         getDriver().findElement(By.id("tasks")).findElement(By.linkText("People")).click();
-        getWait5().until(ExpectedConditions
-                .visibilityOfElementLocated(By.id("people")))
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("people")))
                 .findElement(By.xpath("//a[contains(@href, '/user/')]"))
                 .click();
-        getWait5().until(ExpectedConditions
-                .presenceOfElementLocated(By.id("tasks")))
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.id("tasks")))
                 .findElement(By.linkText("Configure"))
                 .click();
     }
 
     private String[] getTokenUuidUser(String projectName) {
-        getWait5().until(ExpectedConditions
-                        .elementToBeClickable(By.xpath("//button[text()='Add new Token']")))
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Add new Token']")))
                 .click();
         getDriver().findElement(By.name("tokenName")).sendKeys(projectName);
         getDriver().findElement(By.id("api-token-property-token-save")).click();
 
-        final String token = getWait5().until(ExpectedConditions
-                .visibilityOfElementLocated(By.xpath("//span[@class='new-token-value visible']")))
+        final String token = getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[@class='new-token-value visible']")))
                 .getText();
         final String uuid = getDriver().findElement(By.name("tokenUuid")).getAttribute("value");
         final String user = getDriver().getCurrentUrl().split("/")[4];
@@ -49,25 +46,22 @@ public class JobRemoteTriggeringAJTest extends BaseTest {
     private void createFreestyleProjectWithConfigurations(String projectName) {
         getDriver().findElement(By.id("jenkins-name-icon")).click();
         getWait5().until(ExpectedConditions.textToBe(By.xpath("//h1"), "Welcome to Jenkins!"));
-        getWait5().until(ExpectedConditions.
-                elementToBeClickable(By.linkText("New Item")))
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.linkText("New Item")))
                 .click();
-        getWait5().until(ExpectedConditions
-                .presenceOfElementLocated(By.name("name")))
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.name("name")))
                 .sendKeys(projectName);
         getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
-        getWait2().until(ExpectedConditions
-                .elementToBeClickable(By.id("ok-button")))
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.id("ok-button")))
                 .click();
 
         scrollToElement(By.id("build-triggers"));
         getDriver().findElement(By.cssSelector("span:has(input[name='pseudoRemoteTrigger'])")).click();
-        getWait2().until(ExpectedConditions
-                .visibilityOfElementLocated(By.name("authToken")))
-                .sendKeys(projectName);
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("authToken"))).sendKeys(projectName);
 
         scrollToElement(By.id("build-environment"));
-        getDriver().findElement(By.cssSelector("span:has(input[name='hudson-plugins-timestamper-TimestamperBuildWrapper'])"))
+        getDriver().findElement(
+                By.cssSelector("span:has(input[name='hudson-plugins-timestamper-TimestamperBuildWrapper'])")
+                )
                 .click();
 
         getDriver().findElement(By.name("Submit")).click();
@@ -75,13 +69,11 @@ public class JobRemoteTriggeringAJTest extends BaseTest {
 
     private void scrollToElement(By by) {
         WebElement element = getDriver().findElement(by);
-        ((JavascriptExecutor)getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
     }
 
     private void triggerJobViaHTTPRequest(String token, String user, String projectName) {
         final String postBuildJob = "http://" + user + ":" + token + "@localhost:8080/job/Project1/build?token=" + projectName;
-
-        //http://iz:110ba851d727a496af6ed2ab39cf0b4d32@localhost:8080/job/Project1/build?token=Project1
 
         getDriver().switchTo().newWindow(WindowType.TAB);
         getDriver().navigate().to(postBuildJob);
@@ -115,23 +107,15 @@ public class JobRemoteTriggeringAJTest extends BaseTest {
         final String uuid = tokenUuidUser[1];
         final String user = tokenUuidUser[2];
 
-        System.out.println(token);
-        System.out.println(uuid);
-        System.out.println(user);
-
         createFreestyleProjectWithConfigurations(projectName);
 
         triggerJobViaHTTPRequest(token, user, projectName);
 
-        getWait60().until(ExpectedConditions
-                        .elementToBeClickable(By.xpath("//a[@tooltip='Success > Console Output']")))
-                        .click();
+        getWait60().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@tooltip='Success > Console Output']")))
+                .click();
 
-        final String actualConsoleLogs = getWait5().until(ExpectedConditions
-                .visibilityOfElementLocated(By.className("console-output")))
+        final String actualConsoleLogs = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("console-output")))
                 .getText();
-
-        System.out.println(actualConsoleLogs);
 
         revokeTokenViaHTTPRequest(token, uuid, user);
 
@@ -146,7 +130,6 @@ public class JobRemoteTriggeringAJTest extends BaseTest {
 
         openUserConfigurations();
         final String emptyTokenMessage = getDriver().findElement(By.cssSelector(".token-list-item>div")).getText();
-        System.out.println(emptyTokenMessage);
 
         Assert.assertEquals(emptyTokenMessage, "There are no registered tokens for this user.");
     }
