92448e3dbbba42074c062f664ac5f7c5672cf6b8
MargaritaArcady <alquavein@gmail.com>
2024-04-27 16:35:51 -0400

    TС_04.003.04 | Folder > Delete Folder fixed problem

--- src/test/java/school/redrover/DeleteFolderTest.java
+++ src/test/java/school/redrover/DeleteFolderTest.java
@@ -1,9 +1,13 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
 
 public class DeleteFolderTest extends BaseTest {
     @Test
@@ -13,10 +17,26 @@ public class DeleteFolderTest extends BaseTest {
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
-    }    public void testDeleteFolder (){
-        getDriver().findElement(By.className("task-link  confirmation-link")).click();
-        getDriver().findElement(By.className("jenkins-button jenkins-button--primary")).click();
+        TestUtils.returnToDashBoard(this);
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//*[@id=\"job_Folder_1\"]/td[3]/a/span")).getText(),
+                "Folder_1");
+    }
+    @Test(dependsOnMethods = "testCreateFolder")
+    public void testDeleteFolder (){
+        getDriver().findElement(By.xpath("//*[@id=\"job_Folder_1\"]/td[3]/a/span")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[4]/span/a/span[2]")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"jenkins\"]/dialog/div[3]/button[1]")).click();
 
-        Assert.assertFalse(getDriver().findElement(By.name("Welcome to Jenkins!")).isDisplayed());
+        boolean folderIsDisplayed;
+        try {
+            WebElement folder = getDriver().findElement(By.xpath("//*[@id=\"job_Folder_1\"]/td[3]/a/span"));
+            folderIsDisplayed = folder.isDisplayed();
+        } catch (NoSuchElementException e) {
+            folderIsDisplayed = false;
+        }
+
+        Assert.assertFalse(folderIsDisplayed);
     }
 }
+
