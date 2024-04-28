348d810373182bfde624097c7f60ecdc798d9d1c
Eugene <dorick23@gmail.com>
2024-04-28 17:37:04 +0300

    TC_02.008.02 | Pipeline > Rename via breadcrumbs

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -1,6 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -8,11 +10,16 @@ import school.redrover.runner.BaseTest;
 public class Pipeline2Test extends BaseTest {
 
     private static final String PIPELINE_NAME = "Created Pipeline";
+    private static final String NEW_PIPELINE_NAME = "New Pipeline name";
     private static final String DESCRIPTION = "Lorem ipsum dolor sit amet";
     private static final By CHANGE_DESCRIPTION_LOCATOR = By.id("description-link");
     private static final By DESCRIPTION_INPUT_LOCATOR = By.name("description");
     private static final By SAVE_BUTTON_LOCATOR = By.name("Submit");
-    private static final By DISPLAYED_DESCRIPTION = By.cssSelector("#description>:first-child");
+    private static final By DISPLAYED_DESCRIPTION_LOCATOR = By.cssSelector("#description>:first-child");
+    private static final By NAME_IN_BREADCRUMBS_LOCATOR = By.cssSelector("li > a[href^='/job/']");
+    private static final By NEW_NAME_INPUT_LOCATOR = By.name("newName");
+    private static final By RENAME_BUTTON_LOCATOR = By.cssSelector("a[href$='rename']");
+
 
     private void createPipeline() {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
@@ -29,7 +36,7 @@ public class Pipeline2Test extends BaseTest {
         getDriver().findElement(DESCRIPTION_INPUT_LOCATOR).sendKeys(DESCRIPTION);
         getDriver().findElement(SAVE_BUTTON_LOCATOR).click();
 
-        Assert.assertEquals(getDriver().findElement(DISPLAYED_DESCRIPTION).getText(), DESCRIPTION);
+        Assert.assertEquals(getDriver().findElement(DISPLAYED_DESCRIPTION_LOCATOR).getText(), DESCRIPTION);
     }
 
     @Test(dependsOnMethods = "testAddDescription")
@@ -43,27 +50,45 @@ public class Pipeline2Test extends BaseTest {
         getDriver().findElement(DESCRIPTION_INPUT_LOCATOR).sendKeys(addedToDescription);
         getDriver().findElement(SAVE_BUTTON_LOCATOR).click();
 
-        Assert.assertEquals(getDriver().findElement(DISPLAYED_DESCRIPTION).getText(), expectedDescription);
+        Assert.assertEquals(getDriver().findElement(DISPLAYED_DESCRIPTION_LOCATOR).getText(), expectedDescription);
     }
 
     @Test
     public void testVerifyNameInBreadcrumbs() {
         createPipeline();
-        String nameInBreadcrumbs = getDriver().findElement(By.cssSelector("li > a[href^='/job/']")).getText();
+        String nameInBreadcrumbs = getDriver().findElement(NAME_IN_BREADCRUMBS_LOCATOR).getText();
 
         Assert.assertEquals(nameInBreadcrumbs, PIPELINE_NAME);
     }
 
     @Test
     public void testRenameJobViaSidebar() {
-        final String newName = "New pipeline name";
-
         createPipeline();
-        getDriver().findElement(By.cssSelector("a[href$='rename']")).click();
-        getDriver().findElement(By.name("newName")).clear();
-        getDriver().findElement(By.name("newName")).sendKeys(newName);
+        getDriver().findElement(RENAME_BUTTON_LOCATOR).click();
+        getDriver().findElement(NEW_NAME_INPUT_LOCATOR).clear();
+        getDriver().findElement(NEW_NAME_INPUT_LOCATOR).sendKeys(NEW_PIPELINE_NAME);
         getDriver().findElement(SAVE_BUTTON_LOCATOR).click();
 
-        Assert.assertEquals(getDriver().findElement(By.cssSelector("div > h1")).getText(), newName);
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("div > h1")).getText(), NEW_PIPELINE_NAME);
+    }
+
+    @Test
+    public void testRenameViaDropdown(){
+        createPipeline();
+
+        new Actions(getDriver())
+                .moveToElement(getDriver().findElement(NAME_IN_BREADCRUMBS_LOCATOR))
+                .perform();
+
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By
+                .cssSelector("a[href^='/job'] > button"))).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".tippy-content")));
+
+        getDriver().findElement(By.cssSelector("div > a[href$='rename']")).click();
+        getDriver().findElement(NEW_NAME_INPUT_LOCATOR).clear();
+        getDriver().findElement(NEW_NAME_INPUT_LOCATOR).sendKeys(NEW_PIPELINE_NAME);
+        getDriver().findElement(SAVE_BUTTON_LOCATOR).click();
+
+        Assert.assertEquals(getDriver().findElement(NAME_IN_BREADCRUMBS_LOCATOR).getText(), NEW_PIPELINE_NAME);
     }
 }
