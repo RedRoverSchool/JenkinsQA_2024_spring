adaa834fb043e65f5a41e8b0161c75777e845fd6
Eugene <dorick23@gmail.com>
2024-04-29 00:53:46 +0300

    add exp.cond.

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -84,7 +84,9 @@ public class Pipeline2Test extends BaseTest {
         while (attempts < 3) {
             try {
                 getWait5().until(ExpectedConditions.visibilityOfElementLocated(CHEVRON_LOCATOR));
-                getDriver().findElement(CHEVRON_LOCATOR).click();
+//                getDriver().findElement(CHEVRON_LOCATOR).click();
+                getDriver().findElement(By.cssSelector("[href^='/job'] [class$='dropdown-chevron']")).click();
+                getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".tippy-content")));
                 getDriver().findElement(By.cssSelector("div > a[href$='rename']")).click();
                 getDriver().findElement(NEW_NAME_INPUT_LOCATOR).clear();
                 getDriver().findElement(NEW_NAME_INPUT_LOCATOR).sendKeys(NEW_PIPELINE_NAME);
