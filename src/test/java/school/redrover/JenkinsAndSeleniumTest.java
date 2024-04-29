1e215dc5ccba151781dedfc4479b48bc33234397
Serhii Malko <malsv@ukr.net>
2024-04-30 02:36:41 +0300

    int selectedX = 170;

--- src/test/java/school/redrover/JenkinsAndSeleniumTest.java
+++ src/test/java/school/redrover/JenkinsAndSeleniumTest.java
@@ -33,7 +33,7 @@ public class JenkinsAndSeleniumTest extends BaseTest {
     public void testOffset175() {
 
         int offsetX = -5;
-        int selectedX = 180;
+        int selectedX = 170;
 
         createItemsFromList(PROJECT_NAMES);
         String jobName = TestUtils.FREESTYLE_PROJECT;
