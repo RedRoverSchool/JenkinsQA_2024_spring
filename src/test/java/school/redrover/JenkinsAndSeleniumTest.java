e64aafb8065a2374664d4a1d925188b74b078fdc
Serhii Malko <malsv@ukr.net>
2024-04-29 15:29:50 +0300

    contextClick()

--- src/test/java/school/redrover/JenkinsAndSeleniumTest.java
+++ src/test/java/school/redrover/JenkinsAndSeleniumTest.java
@@ -2,14 +2,13 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class JenkinsAndSeleniumTest extends BaseTest {
@@ -18,11 +17,8 @@ public class JenkinsAndSeleniumTest extends BaseTest {
     private List<String> getNamesList() {
         List<String> names = new ArrayList<>();
         names.add(TestUtils.FREESTYLE_PROJECT);
-        names.add(TestUtils.MULTIBRANCH_PIPELINE);
-        names.add(TestUtils.ORGANIZATION_FOLDER);
         names.add(TestUtils.FOLDER);
         names.add(TestUtils.MULTI_CONFIGURATION_PROJECT);
-        names.add(TestUtils.PIPELINE);
         return names;
     }
 
@@ -33,18 +29,26 @@ public class JenkinsAndSeleniumTest extends BaseTest {
         }
     }
 
-    private List<String> getChevronMenu(String jobName) {
-        TestUtils.openElementDropdown(this, getDriver().findElement(By.linkText(jobName)));
-
-        WebElement dropdownMenu = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//div[@class='jenkins-dropdown']")));
-
-        return Arrays.stream(dropdownMenu.getText().split("\\r?\\n")).toList();
-    }
+//    private List<String> getChevronMenu(String jobName) {
+//        TestUtils.openElementDropdown(this, getDriver().findElement(By.linkText(jobName)));
+//
+//        WebElement dropdownMenu = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+//                By.xpath("//div[@class='jenkins-dropdown']")));
+//
+//        return Arrays.stream(dropdownMenu.getText().split("\\r?\\n")).toList();
+//    }
 
     @Test
     public void testSelenium() {
         createItemsFromList(projectsNames);
+
+        WebElement job = getDriver().findElement(By.linkText(TestUtils.FREESTYLE_PROJECT));
+        Actions actions = new Actions(getDriver());
+
+        actions
+                .moveToElement(job)
+                .contextClick()
+                .perform();
 
         Assert.assertEquals(1, 2);
 
