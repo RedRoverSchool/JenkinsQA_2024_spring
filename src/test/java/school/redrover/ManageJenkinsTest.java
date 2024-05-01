ad57be31382d142fc1ba8219aea544ebd489bfec
KathyGU <dendritsm@gmail.com>
2024-05-01 07:56:02 +0300

    TC_09.002.02: verify section names of security block

--- src/test/java/school/redrover/ManageJenkinsTest.java
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -1,9 +1,13 @@
 package school.redrover;
 
+import org.checkerframework.checker.signature.qual.DotSeparatedIdentifiersOrPrimitiveType;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.util.List;
 
 public class ManageJenkinsTest extends BaseTest {
 
@@ -14,5 +18,20 @@ public class ManageJenkinsTest extends BaseTest {
 
         String pageTitle = getDriver().getTitle().split(" ")[0];
         Assert.assertEquals(pageTitle, "Security");
+    }
+
+    @Test
+    public void testSectionNamesOfSecurityBlock() {
+        final List <String> sectionNames = List.of("Security", "Manage Credentials", "Configure Credential Providers",
+                "Users", "In-process Script Approval");
+
+        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
+
+        List <WebElement> securityBlockElements = getDriver().findElements(By
+                .xpath("//section[contains(@class, 'jenkins-section')][2]//div//dt"));
+
+        for(int i = 0; i < securityBlockElements.size(); i++) {
+            Assert.assertTrue(securityBlockElements.get(i).getText().matches(sectionNames.get(i)));
+        }
     }
 }
