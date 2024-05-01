7be5cadd6ffbd7f912c8b62287635ad77c9e61fb
Dr.Johanson <doc.johanson@gmail.com>
2024-05-01 22:14:25 +0500

    TC_09.002.04-View-Manage-Jenkins-page-Visibility-and-clickability-Verify-visibility-of-sections-names

--- src/test/java/school/redrover/ManageJenkinsTest.java
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -5,11 +5,36 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import org.testng.Assert;
-import school.redrover.runner.BaseTest;
+
 import java.util.List;
 
 public class ManageJenkinsTest extends BaseTest {
+
+    private static final List<String> SYSTEM_CONFIGURATION_SECTIONS_NAMES_EXPECTED = List.of("System", "Tools", "Plugins", "Nodes", "Clouds", "Appearance");
+    private static final List<String> SECURITY_SECTIONS_NAMES_EXPECTED = List.of("Security", "Credentials", "Credential Providers", "Users", "In-process Script Approval");
+    private static final List<String> STATUS_INFORMATION_SECTIONS_NAMES_EXPECTED = List.of("System Information", "System Log", "Load Statistics", "About Jenkins");
+    private static final List<String> TROUBLESHOOTING_SECTIONS_NAMES_EXPECTED = List.of("Manage Old Data");
+    private static final List<String> TOOLS_AND_ACTIONS_SECTIONS_NAMES_EXPECTED = List.of("Reload Configuration from Disk", "Jenkins CLI", "Script Console", "Prepare for Shutdown");
+    private static final List<String> BLOCK_NAMES_EXPECTED = List.of("System Configuration", "Security", "Status Information", "Troubleshooting", "Tools and Actions");
+    private static final int SYSTEM_BLOCK = 1;
+    private static final int SECURITY_BLOCK= 2;
+    private static final int STATUS_BLOCK = 3;
+    private static final int TROUBLESHOOTING_BLOCK = 4;
+    private static final int TOOLS_BLOCK = 5;
+
+    private void goToManage() {
+        getDriver().findElement(By.cssSelector("[href='/manage']")).click();
+    }
+
+    private By getBlockLocator(int blockNumber) {
+        return By.xpath("//section[contains(@class, 'jenkins-section')][" + blockNumber + "]//div//dt");
+    }
+
+    private List<List<String>> getSectionsNamesListExpected() {
+        return List.of(SYSTEM_CONFIGURATION_SECTIONS_NAMES_EXPECTED, SECURITY_SECTIONS_NAMES_EXPECTED,
+                STATUS_INFORMATION_SECTIONS_NAMES_EXPECTED, TROUBLESHOOTING_SECTIONS_NAMES_EXPECTED,
+                TOOLS_AND_ACTIONS_SECTIONS_NAMES_EXPECTED);
+    }
 
     private boolean areElementsEnabled(List<WebElement> elements) {
         for (WebElement element : elements) {
@@ -82,4 +107,28 @@ public class ManageJenkinsTest extends BaseTest {
             Assert.assertTrue(areElementsEnabled(toolsAndActionsSections),
                     "'Tools and Actions' sections are not clickable");
         }
+
+    @Test
+    public void testSectionsNamesVisibility() {
+        goToManage();
+
+        List<WebElement> blockNamesActual = getDriver().findElements(By.cssSelector("[class$='section__title']"));
+
+        List<List<WebElement>> sectionListNamesActual = List.of(getDriver().findElements(getBlockLocator(SYSTEM_BLOCK)),
+                getDriver().findElements(getBlockLocator(SECURITY_BLOCK)),
+                getDriver().findElements(getBlockLocator(STATUS_BLOCK)),
+                getDriver().findElements(getBlockLocator(TROUBLESHOOTING_BLOCK)),
+                getDriver().findElements(getBlockLocator(TOOLS_BLOCK)));
+
+        for (int i = 0; i < BLOCK_NAMES_EXPECTED.size(); i++) {
+            Assert.assertTrue(blockNamesActual.get(i).getText().matches(BLOCK_NAMES_EXPECTED.get(i)));
+        }
+
+        for (int i = 0; i < getSectionsNamesListExpected().size(); i++) {
+            for (int j = 0; j < getSectionsNamesListExpected().get(i).size(); j++) {
+                Assert.assertTrue(sectionListNamesActual.get(i).get(j).getText()
+                        .matches(getSectionsNamesListExpected().get(i).get(j)));
+            }
+        }
     }
+}
