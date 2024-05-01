e1a9111ffb401bfbc0e465d8dff628e5b560ebde
kas-113 <allakravtsova.113@gmail.com>
2024-05-02 00:44:08 +0200

    TC_02.004.19 Pipeline > Pipeline Configuration for the existing Pipeline > Set negative Quiet period for BuildTriggers in General section

--- src/test/java/school/redrover/PipelineConfigurationTest.java
+++ src/test/java/school/redrover/PipelineConfigurationTest.java
@@ -11,7 +11,6 @@ import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.TestUtils;
 
 import java.util.List;
 
