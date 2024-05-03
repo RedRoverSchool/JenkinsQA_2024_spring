8ae0f3ec99a4bcee896b9f2d9e08e84f7d47f7e9
SerHemb <iserhiy888@gmail.com>
2024-05-03 11:01:37 -0500

    RF_12.001.02 | Footer > Jenkins version

--- src/test/java/school/redrover/model/base/BasePage.java
+++ src/test/java/school/redrover/model/base/BasePage.java
@@ -2,9 +2,14 @@ package school.redrover.model.base;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.HomePage;
 
 public abstract class BasePage extends BaseModel {
+
+    @FindBy(xpath = "//button[@type='button']")
+    private WebElement footerVersion;
 
     public BasePage(WebDriver driver) {
         super(driver);
