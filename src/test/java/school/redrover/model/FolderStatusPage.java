19548ed8eb6d5b39d6a2b4c4578fd525f27cc6b2
Serhii Malko <malsv@ukr.net>
2024-05-05 03:48:13 +0300

    edit FolderStatusPage

--- src/test/java/school/redrover/model/FolderStatusPage.java
+++ src/test/java/school/redrover/model/FolderStatusPage.java
@@ -10,6 +10,9 @@ public class FolderStatusPage extends BasePage {
     @FindBy(css = "[class*='breadcrumbs']>[href*='job']")
     private WebElement breadcrumbsName;
 
+    @FindBy(css = "h1")
+    private WebElement pageTopic;
+
     public FolderStatusPage(WebDriver driver) {
         super(driver);
     }
@@ -17,4 +20,9 @@ public class FolderStatusPage extends BasePage {
     public String getBreadcrumbName() {
         return breadcrumbsName.getText();
     }
+
+    public String getPageTopic() {
+        return pageTopic.getText();
+    }
+
 }
