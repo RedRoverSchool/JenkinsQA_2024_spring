7ce62964aaa3638d7289f7100d56ac35d75fd588
Serhii Malko <malsv@ukr.net>
2024-05-05 03:49:13 +0300

    RF Folder1Test - testCreate

--- src/test/java/school/redrover/Folder1Test.java
+++ src/test/java/school/redrover/Folder1Test.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
 
@@ -13,10 +14,22 @@ public class Folder1Test extends BaseTest {
     private static final String SECOND_FOLDER_NAME = "Inner Folder 2";
 
     @Test
-    public void testCreate() {
+    public void testCreate1() {
         TestUtils.createItem(TestUtils.FOLDER, ROOT_FOLDER_NAME, this);
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), ROOT_FOLDER_NAME);
+    }
+
+    @Test
+    public void testCreate() {
+        String pageTopic = new HomePage(getDriver())
+                .clickNewItem()
+                .setItemName(ROOT_FOLDER_NAME)
+                .selectFolderAndClickOk()
+                .clickSaveButton()
+                .getPageTopic();
+
+        Assert.assertEquals(pageTopic, ROOT_FOLDER_NAME);
     }
 
     @Test(dependsOnMethods = "testCreate")
