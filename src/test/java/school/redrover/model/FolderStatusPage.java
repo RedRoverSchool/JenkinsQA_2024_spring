667c55573f289a189c90006294f69c4f390bf339
Serhii Malko <malsv@ukr.net>
2024-05-05 04:55:59 +0300

    add methods to FolderStatusPage

--- src/test/java/school/redrover/model/FolderStatusPage.java
+++ src/test/java/school/redrover/model/FolderStatusPage.java
@@ -1,9 +1,12 @@
 package school.redrover.model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BasePage;
+
+import java.util.List;
 
 public class FolderStatusPage extends BasePage {
 
@@ -12,6 +15,9 @@ public class FolderStatusPage extends BasePage {
 
     @FindBy(css = "h1")
     private WebElement pageTopic;
+
+    @FindBy(css = ".empty-state-section")
+    private WebElement emptyStateSection;
 
     public FolderStatusPage(WebDriver driver) {
         super(driver);
@@ -24,5 +30,23 @@ public class FolderStatusPage extends BasePage {
     public String getPageTopic() {
         return pageTopic.getText();
     }
+
+    public Boolean isFolderEmpty() {
+        return emptyStateSection.isDisplayed();
+    }
+
+    public CreateNewItemPage clickNewItemInsideFolder() {
+        getDriver().findElement(By.xpath("//a[.='New Item']")).click();
+
+        return new CreateNewItemPage(getDriver());
+    }
+
+    public List<String> getItemListInsideFolder() {
+        return getDriver().findElements(By.cssSelector("tr > td > .jenkins-table__link > span:first-child"))
+                .stream()
+                .map(WebElement::getText)
+                .toList();
+    }
+
 
 }
