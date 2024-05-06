f72ee881a25fb322bc56e5d04d7bc0bfadd42879
iKuper <amalgammy@mail.ru>
2024-05-06 23:10:55 +0300

    RF_09.005.01 | Manage Jenkins > Appearance > Verify quantity of Themes via Dropdown

--- src/test/java/school/redrover/model/AppearancePage.java
+++ src/test/java/school/redrover/model/AppearancePage.java
@@ -3,17 +3,19 @@ package school.redrover.model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BasePage;
 
 import java.util.List;
 
 public class AppearancePage extends BasePage {
+
+    @FindBy(className = "app-theme-picker__item")
+    private List<WebElement> themesList;
+
     public AppearancePage(WebDriver driver) {
         super(driver);
     }
 
-    public List<WebElement> getThemesList() {
-
-        return getDriver().findElements(By.className("app-theme-picker__item"));
-    }
+    public List<WebElement> getThemesList() { return themesList; }
 }
