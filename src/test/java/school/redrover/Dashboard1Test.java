ec564f0e42873ba3d848dd1b93c496fce2c00c61
Serhii Malko <malsv@ukr.net>
2024-05-07 03:53:03 +0300

    RF Dashboaed1Test

--- src/test/java/school/redrover/Dashboard1Test.java
+++ src/test/java/school/redrover/Dashboard1Test.java
@@ -6,104 +6,80 @@ import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Dashboard1Test extends BaseTest {
     private final String VIEW_NAME = "RedRover";
-    private final List<String> projectsNames = getNamesList();
+    private final List<String> PROJECTS_NAMES = Arrays.asList(
+            TestUtils.FREESTYLE_PROJECT,
+            TestUtils.MULTIBRANCH_PIPELINE,
+            TestUtils.ORGANIZATION_FOLDER,
+            TestUtils.FOLDER,
+            TestUtils.MULTI_CONFIGURATION_PROJECT,
+            TestUtils.PIPELINE);
 
-    private List<String> getNamesList() {
-        List<String> names = new ArrayList<>();
-        names.add(TestUtils.FREESTYLE_PROJECT);
-        names.add(TestUtils.MULTIBRANCH_PIPELINE);
-        names.add(TestUtils.ORGANIZATION_FOLDER);
-        names.add(TestUtils.FOLDER);
-        names.add(TestUtils.MULTI_CONFIGURATION_PROJECT);
-        names.add(TestUtils.PIPELINE);
-        return names;
-    }
+    private final List<String> FREESTYLE_PROJECT_MENU = List.of(
+            "Changes",
+            "Workspace",
+            "Build Now",
+            "Configure",
+            "Delete Project",
+            "Move",
+            "Rename");
 
-    private List<String> getFreestyleProjectMenu() {
-        List<String> menu = new ArrayList<>();
-        menu.add("Changes");
-        menu.add("Workspace");
-        menu.add("Build Now");
-        menu.add("Configure");
-        menu.add("Delete Project");
-        menu.add("Move");
-        menu.add("Rename");
-        return menu;
-    }
+    private final List<String> PIPLINE_MENU = List.of(
+            "Changes",
+            "Build Now",
+            "Configure",
+            "Delete Pipeline",
+            "Move",
+            "Full Stage View",
+            "Rename",
+            "Pipeline Syntax");
 
-    private List<String> getPipelineMenu() {
-        List<String> menu = new ArrayList<>();
-        menu.add("Changes");
-        menu.add("Build Now");
-        menu.add("Configure");
-        menu.add("Delete Pipeline");
-        menu.add("Move");
-        menu.add("Full Stage View");
-        menu.add("Rename");
-        menu.add("Pipeline Syntax");
-        return menu;
-    }
+    private final List<String> MULTI_CONFIGURATION_PROJECT_MENU = List.of(
+            "Changes",
+            "Workspace",
+            "Build Now",
+            "Configure",
+            "Delete Multi-configuration project",
+            "Move",
+            "Rename");
 
-    private List<String> getMultiConfigurationProjectMenu() {
-        List<String> menu = new ArrayList<>();
-        menu.add("Changes");
-        menu.add("Workspace");
-        menu.add("Build Now");
-        menu.add("Configure");
-        menu.add("Delete Multi-configuration project");
-        menu.add("Move");
-        menu.add("Rename");
-        return menu;
-    }
+    private final List<String> FOLDER_MENU = List.of(
+            "Configure",
+            "New Item",
+            "Delete Folder",
+            "People",
+            "Build History",
+            "Rename",
+            "Credentials");
 
-    private List<String> getFolderMenu() {
-        List<String> menu = new ArrayList<>();
-        menu.add("Configure");
-        menu.add("New Item");
-        menu.add("Delete Folder");
-        menu.add("People");
-        menu.add("Build History");
-        menu.add("Rename");
-        menu.add("Credentials");
-        return menu;
-    }
+    private final List<String> MULTIBRANCH_PIPELINE_MENU = List.of(
+            "Configure",
+            "Scan Multibranch Pipeline Log",
+            "Multibranch Pipeline Events",
+            "Delete Multibranch Pipeline",
+            "People",
+            "Build History",
+            "Move",
+            "Rename",
+            "Pipeline Syntax",
+            "Credentials");
 
-    private List<String> getMultibranchPipelineMenu() {
-        List<String> menu = new ArrayList<>();
-        menu.add("Configure");
-        menu.add("Scan Multibranch Pipeline Log");
-        menu.add("Multibranch Pipeline Events");
-        menu.add("Delete Multibranch Pipeline");
-        menu.add("People");
-        menu.add("Build History");
-        menu.add("Move");
-        menu.add("Rename");
-        menu.add("Pipeline Syntax");
-        menu.add("Credentials");
-        return menu;
-    }
-
-    private List<String> getOrganizationFolderMenu() {
-        List<String> menu = new ArrayList<>();
-        menu.add("Configure");
-        menu.add("Scan Organization Folder Log");
-        menu.add("Organization Folder Events");
-        menu.add("Delete Organization Folder");
-        menu.add("People");
-        menu.add("Build History");
-        menu.add("Move");
-        menu.add("Rename");
-        menu.add("Pipeline Syntax");
-        menu.add("Credentials");
-        return menu;
-    }
+    private final List<String> ORGANIZATION_FOLDER_MENU = List.of(
+            "Configure",
+            "Scan Organization Folder Log",
+            "Organization Folder Events",
+            "Delete Organization Folder",
+            "People",
+            "Build History",
+            "Move",
+            "Rename",
+            "Pipeline Syntax",
+            "Credentials");
 
     public void createItem(String type, String name) {
         new HomePage(getDriver())
@@ -111,12 +87,6 @@ public class Dashboard1Test extends BaseTest {
                 .setItemName(name)
                 .selectTypeAndClickOk(type)
                 .clickLogo();
-    }
-
-    private List<String> getItemNamesAfterSortingByName() {
-        return new HomePage(getDriver())
-                .clickColumnNameTitle()
-                .getItemList();
     }
 
     private List<String> getChevronMenu(String jobName) {
@@ -129,34 +99,54 @@ public class Dashboard1Test extends BaseTest {
     }
 
     @Test
+    public void testSortItemsByName() {
+        PROJECTS_NAMES.forEach(s -> createItem(s, s));
+
+        Collections.sort(PROJECTS_NAMES);
+
+        Collections.reverse(PROJECTS_NAMES);
+        List<String> descSortingByNameList = new HomePage(getDriver())
+                .clickColumnNameTitle()
+                .getItemList();
+
+        Assert.assertEquals(descSortingByNameList, PROJECTS_NAMES);
+
+        Collections.reverse(PROJECTS_NAMES);
+        List<String> ascSortingByNameList = new HomePage(getDriver())
+                .clickColumnNameTitle()
+                .getItemList();
+
+        Assert.assertEquals(ascSortingByNameList, PROJECTS_NAMES);
+    }
+
+    @Test(dependsOnMethods = "testSortItemsByName")
     public void testFreestyleProjectChevronMenu() {
-        projectsNames.forEach(s -> createItem(s, s));
-        Assert.assertEquals(getChevronMenu(TestUtils.FREESTYLE_PROJECT), getFreestyleProjectMenu());
+        Assert.assertEquals(getChevronMenu(TestUtils.FREESTYLE_PROJECT), FREESTYLE_PROJECT_MENU);
     }
 
     @Test(dependsOnMethods = "testFreestyleProjectChevronMenu")
     public void testPipelineChevronMenu() {
-        Assert.assertEquals(getChevronMenu(TestUtils.PIPELINE), getPipelineMenu());
+        Assert.assertEquals(getChevronMenu(TestUtils.PIPELINE), PIPLINE_MENU);
     }
 
     @Test(dependsOnMethods = "testPipelineChevronMenu")
     public void testMultiConfigurationProjectChevronMenu() {
-        Assert.assertEquals(getChevronMenu(TestUtils.MULTI_CONFIGURATION_PROJECT), getMultiConfigurationProjectMenu());
+        Assert.assertEquals(getChevronMenu(TestUtils.MULTI_CONFIGURATION_PROJECT), MULTI_CONFIGURATION_PROJECT_MENU);
     }
 
     @Test(dependsOnMethods = "testMultiConfigurationProjectChevronMenu")
     public void testFolderChevronMenu() {
-        Assert.assertEquals(getChevronMenu(TestUtils.FOLDER), getFolderMenu());
+        Assert.assertEquals(getChevronMenu(TestUtils.FOLDER), FOLDER_MENU);
     }
 
     @Test(dependsOnMethods = "testFolderChevronMenu")
     public void testMultibranchPipelineChevronMenu() {
-        Assert.assertEquals(getChevronMenu(TestUtils.MULTIBRANCH_PIPELINE), getMultibranchPipelineMenu());
+        Assert.assertEquals(getChevronMenu(TestUtils.MULTIBRANCH_PIPELINE), MULTIBRANCH_PIPELINE_MENU);
     }
 
     @Test(dependsOnMethods = "testMultibranchPipelineChevronMenu")
     public void testOrganizationChevronMenu() {
-        Assert.assertEquals(getChevronMenu(TestUtils.ORGANIZATION_FOLDER), getOrganizationFolderMenu());
+        Assert.assertEquals(getChevronMenu(TestUtils.ORGANIZATION_FOLDER), ORGANIZATION_FOLDER_MENU);
     }
 
     @Test(dependsOnMethods = "testOrganizationChevronMenu")
@@ -173,23 +163,11 @@ public class Dashboard1Test extends BaseTest {
     }
 
     @Test(dependsOnMethods = "testCreateView")
-    public void testSortItemsByName() {
-        Collections.sort(projectsNames);
-        Collections.reverse(projectsNames);
-        boolean isDescendingSortingOk = getItemNamesAfterSortingByName().equals(projectsNames);
-
-        Collections.reverse(projectsNames);
-        boolean isAscendingSortingOk = getItemNamesAfterSortingByName().equals(projectsNames);
-
-        Assert.assertTrue(isAscendingSortingOk && isDescendingSortingOk);
-    }
-
-    @Test(dependsOnMethods = "testSortItemsByName")
     public void testAddItemsToView() {
         final String SELECTED_NAME1 = TestUtils.MULTIBRANCH_PIPELINE;
         final String SELECTED_NAME2 = TestUtils.FOLDER;
 
-        List<String> namesFromNewView = new HomePage(getDriver())
+        List<String> namesFromView = new HomePage(getDriver())
                 .clickViewName(VIEW_NAME)
                 .clickEditViewButton()
                 .selectProjectForAddToView(SELECTED_NAME1)
@@ -197,10 +175,9 @@ public class Dashboard1Test extends BaseTest {
                 .clickOkButton()
                 .getProjectNames();
 
-        boolean isName1InView = namesFromNewView.contains(SELECTED_NAME1);
-        boolean isName2InView = namesFromNewView.contains(SELECTED_NAME2);
-
-        Assert.assertTrue(namesFromNewView.size() == 2 && isName1InView && isName2InView);
+        Assert.assertTrue(namesFromView.size() == 2
+                && namesFromView.contains(SELECTED_NAME1)
+                && namesFromView.contains(SELECTED_NAME2));
     }
 
     @Test(dependsOnMethods = "testAddItemsToView")
