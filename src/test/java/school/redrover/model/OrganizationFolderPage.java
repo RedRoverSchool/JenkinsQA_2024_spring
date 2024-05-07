3d032c087240f6c6c0ad1f8fe91c756f900ccd54
Maksim Goncharov <maxg131203@gmail.com>
2024-05-06 19:39:53 -0700

    TC_06.001.01 | Organization Folder > Configuration > Confirm visibility of the sidebar menu

--- src/test/java/school/redrover/model/OrganizationFolderPage.java
+++ src/test/java/school/redrover/model/OrganizationFolderPage.java
@@ -1,11 +1,27 @@
 package school.redrover.model;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BasePage;
 
 public class OrganizationFolderPage extends BasePage {
 
+    @FindBy(id = "tasks")
+    private WebElement sidebarMenu;
+
+    @FindBy(css = "span > a[href$='configure']")
+    private WebElement configureButton;
+
     public OrganizationFolderPage(WebDriver driver) {
         super(driver);
+    }
+
+    public boolean isSidebarVisible() {
+        return sidebarMenu.isDisplayed();
+    }
+
+    public void clickConfigure() {
+        configureButton.click();
     }
 }
