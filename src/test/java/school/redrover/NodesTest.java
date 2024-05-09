e42e0751686e28055f32a5788fd815c528cdf656
Denis Prokofyev <denisprokofyev@gmail.com>
2024-05-09 22:04:38 +0300

    RF_UniteNodesTests

--- src/test/java/school/redrover/NodesTest.java
+++ src/test/java/school/redrover/NodesTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -31,18 +30,6 @@ public class NodesTest extends BaseTest {
         getDriver().findElement(By.name("Submit")).click();
     }
 
-    public void createNode() {
-
-        final String nodName = "TestNode";
-
-        getDriver().findElement(By.linkText("Manage Jenkins")).click();
-        getDriver().findElement(By.xpath("//a[@href='computer']")).click();
-        getDriver().findElement(By.xpath("//a[@href='new']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(nodName);
-        getDriver().findElement(By.xpath("//label[@class='jenkins-radio__label']")).click();
-        getDriver().findElement(By.name("Submit")).click();
-        getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate' and @name='Submit']")).click();
-    }
     @Test
     public void testAddNode() {
         String text;
