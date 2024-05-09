3938397a45d0c69ea403120bb23c8cc18707801e
iKuper <amalgammy@mail.ru>
2024-05-09 23:24:57 +0300

    RF | code fix

--- src/test/java/school/redrover/model/HomePage.java
+++ src/test/java/school/redrover/model/HomePage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BasePage;
-import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
 
 import java.util.ArrayList;
