12a8af7f1fc80e73b7d1ac9ae60f48e844b418aa
iKuper <amalgammy@mail.ru>
2024-05-10 19:22:43 +0300

    refactor TestUtils

--- src/test/java/school/redrover/CreateFolder1Test.java
+++ src/test/java/school/redrover/CreateFolder1Test.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
