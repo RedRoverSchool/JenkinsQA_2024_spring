2bdf2d2bfd0c21b7cf22eb97d407eedbaa4a07ef
SvitlanaNev <sidzp2019@gmail.com>
2024-05-10 17:50:05 +0300

    RF_00.002.15 | Pipeline > Refactoring tests to POM

--- src/test/java/school/redrover/PiplineProjectsTest.java
+++ src/test/java/school/redrover/PiplineProjectsTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class PiplineProjectsTest extends BaseTest {
 
-    private final String PIPELINE = "PipelineOne";
+    private final String PIPELINE = "PipelineProject";
 
     @Test
     public void testCreatePipline() {
