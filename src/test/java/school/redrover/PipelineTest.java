35ce7fdcb383251d5cb85fac7a73d6607ba75d4c
Eugene <dorick23@gmail.com>
2024-05-11 16:38:59 +0300

    RF: delete commented test

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -805,19 +805,6 @@ public class PipelineTest extends BaseTest {
         }
     }
 
-//    @Test
-//    public void testNewPipelineProject() {
-//
-//        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-//        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("ProjectPL");
-//        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[2]/label")).click();
-//        getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
-//        getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
-//
-//        Assert.assertEquals(getDriver().findElement(
-//                By.xpath("//*[@id='main-panel']/div[1]/div/h1")).getText(),"ProjectPL");
-//    }
-
     @Test(dependsOnMethods = "testCreatePipeline")
     public void testUseSearchToFindProject() {
 
