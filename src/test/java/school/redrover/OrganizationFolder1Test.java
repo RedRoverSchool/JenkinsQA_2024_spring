b7a6d003f37d8b947f0c6955c45c19a720d28099
Anastasiia Biletska <biletskaanastasiiia@gmail.com>
2024-05-11 19:58:15 -0700

    RF testBuildNowFreestyleProject() to POM

--- src/test/java/school/redrover/OrganizationFolder1Test.java
+++ src/test/java/school/redrover/OrganizationFolder1Test.java
@@ -18,7 +18,6 @@ public class OrganizationFolder1Test extends BaseTest {
                 .clickNewItem()
                 .setItemName(organizationFolderName)
                 .selectOrganizationFolderAndClickOk()
-                .clickSave()
                 .clickLogo()
                 .getItemList();
         Assert.assertTrue(itemList.contains(organizationFolderName));
