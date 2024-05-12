3f49b714e51e421a06bb34e7af1d40686c3c7778
DVlad9393 <vladdd9312@gmail.com>
2024-05-12 22:42:27 +0300

    RF_00.00.007 | Item > Freestyle Project >FreestyleProject4Test

--- src/test/java/school/redrover/FreestyleProject4Test.java
+++ src/test/java/school/redrover/FreestyleProject4Test.java
@@ -9,6 +9,8 @@ import school.redrover.model.FreestyleProjectPage;
 import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
 
+import java.util.List;
+
 public class FreestyleProject4Test extends BaseTest {
 
     private static final String PROJECT_NAME = "JavaHashGroupProject";
@@ -50,48 +52,27 @@ public class FreestyleProject4Test extends BaseTest {
     public void testCreateNewFreestyleProjectWithDescription (){
         final String projectItemDescription = "This is first Project";
 
-        WebElement newItemButton = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@href='/view/all/newJob']")));
-        newItemButton.click();
+        FreestyleProjectPage freestyleProjectPage = new HomePage(getDriver())
+                .clickCreateJob()
+                .setItemName(PROJECT_NAME)
+                .selectFreestyleAndClickOk()
+                .inputDescription(projectItemDescription)
+                .clickSaveButton();
 
-        WebElement inputNameField = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='name']")));
-        inputNameField.sendKeys(PROJECT_NAME);
+        Assert.assertTrue(freestyleProjectPage.isProjectNameDisplayed());
+        Assert.assertEquals(freestyleProjectPage.getProjectName(),PROJECT_NAME);
+        Assert.assertEquals(freestyleProjectPage.getProjectDescriptionText(),projectItemDescription);
 
-        WebElement freestyleProjectButton = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Freestyle project')]")));
-        freestyleProjectButton.click();
+        List<String> itemList = new HomePage(getDriver())
+                .clickLogo()
+                .getItemList();
 
-        WebElement okButton = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
-        okButton.click();
+        Assert.assertTrue((itemList.contains(PROJECT_NAME)));
 
-        WebElement inputDescriptonField = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@name='description']")));
-        inputDescriptonField.sendKeys(projectItemDescription);
+        String description = new HomePage(getDriver()).clickCreatedFreestyleName()
+                        .getProjectDescriptionText();
+        Assert.assertEquals(description,projectItemDescription);
 
-        WebElement saveButton = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@name='Submit']")));
-        saveButton.click();
 
-        WebElement newProjectHeader = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-app-bar__content jenkins-build-caption']")));
-        WebElement newProjectDescription = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'" + projectItemDescription + "')]")));
-
-        Assert.assertTrue(newProjectHeader.isDisplayed());
-        Assert.assertEquals(newProjectHeader.getText(),PROJECT_NAME);
-
-        Assert.assertTrue(newProjectDescription.isDisplayed());
-        Assert.assertEquals(newProjectDescription.getText(),projectItemDescription);
-
-        WebElement mainPageJenkinsButton = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@id='jenkins-head-icon']")));
-        mainPageJenkinsButton.click();
-
-        WebElement mainPageFreestyleProjectNameField = getWait2().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//td/a[@href='job/" + PROJECT_NAME + "/']")));
-
-        Assert.assertEquals(mainPageFreestyleProjectNameField.getText(),PROJECT_NAME);
     }
 }
