610cf26e71e4b536cf5f975696321f0d13ef9e40
Serhii Malko <malsv@ukr.net>
2024-05-13 15:35:21 +0300

    RF CreateFolder3Test.java

--- src/test/java/school/redrover/CreateFolder3Test.java
+++ src/test/java/school/redrover/CreateFolder3Test.java
@@ -1,12 +1,10 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
+
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 
 public class CreateFolder3Test extends BaseTest {
