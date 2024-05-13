f2fe4587fa05eea3c47e6e0bcb48b542d9a77b48
Serhii Malko <malsv@ukr.net>
2024-05-13 22:07:54 +0300

    RF CreateFolder1Test -> POM

--- src/test/java/school/redrover/CreateFolder1Test.java
+++ src/test/java/school/redrover/CreateFolder1Test.java
@@ -1,8 +1,5 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.model.CreateNewItemPage;
