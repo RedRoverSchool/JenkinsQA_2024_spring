efa603df9ab9324ee88be854be0bb6925fe4352c
Serhii Malko <malsv@ukr.net>
2024-05-13 15:31:52 +0300

    CreateFolder3Test -> POM

--- src/test/java/school/redrover/CreateFolder3Test.java
+++ src/test/java/school/redrover/CreateFolder3Test.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -12,12 +13,11 @@ public class CreateFolder3Test extends BaseTest {
 
     @Test
     public void testSpecialCharactersNameFolder(){
-        getDriver().findElement(By.xpath("//a[.='New Item']")).click();
-        getDriver().findElement(By.id("j-add-item-type-nested-projects")).click();
-        getDriver().findElement(By.id("name")).sendKeys("Fold%erdate");
-        WebElement errorMassage = getDriver().findElement(By.id("itemname-invalid"));
+        String errorMessage = new HomePage(getDriver())
+                .clickNewItem()
+                .setItemName("Fold%erdate")
+                .getErrorMessage();
 
-        assertEquals(errorMassage.getText(),"» ‘%’ is an unsafe character");
-        assertTrue(errorMassage.isDisplayed());
+        assertEquals(errorMessage,"» ‘%’ is an unsafe character");
     }
 }
