0ba71f07053bba16b38f22002c6121d3954efee9
iKuper <amalgammy@mail.ru>
2024-05-16 12:08:56 +0300

    RF_00.007.02 | New item > Create a new item from other existing > Check creating Multi-configuration project by copying the exist one > POM

--- src/test/java/school/redrover/MultiConfigurationProjectTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectTest.java
@@ -2,14 +2,12 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.*;
-
 
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
