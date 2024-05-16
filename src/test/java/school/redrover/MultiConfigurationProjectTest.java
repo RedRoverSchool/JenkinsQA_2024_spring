769e807bc6c37fe8473b333d7865fe1372520e55
iKuper <amalgammy@mail.ru>
2024-05-16 12:35:46 +0300

    RF_03.002.05 | Multi-configuration project > Enable / Disable project > Check tooltip enabling project > POM

--- src/test/java/school/redrover/MultiConfigurationProjectTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectTest.java
@@ -143,16 +143,14 @@ public class MultiConfigurationProjectTest extends BaseTest {
 
     @Test(dependsOnMethods = "testMCPDisableByToggle")
     public void testCheckTooltipEnablingMCP() {
-        getDriver().findElement(By.linkText(PROJECT_NAME)).click();
-        getDriver().findElement(By.linkText("Configure")).click();
 
-        new Actions(getDriver())
-                .moveToElement(getDriver().findElement(By.className("jenkins-toggle-switch__label")))
-                .perform();
+        String toggleTooltipText = new HomePage(getDriver())
+                .clickMCPName(PROJECT_NAME)
+                .clickConfigureButton()
+                .hoverOverToggleSwitch()
+                .getToggleTooltipText();
 
-        Assert.assertEquals(
-                getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.tippy-box>div"))).getText(),
-                "Enable or disable the current project");
+        Assert.assertEquals(toggleTooltipText, "Enable or disable the current project");
     }
 
     @Test
