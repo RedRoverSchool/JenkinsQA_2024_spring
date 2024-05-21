919759a2a09ca6f9a2ae74dee5946d3c600f9c27
AllaBird <allavorobieva@yahoo.com>
2024-05-21 01:08:41 -0400

    RF_00.00.017 | Joining the classes

--- src/test/java/school/redrover/NewItemTest.java
+++ src/test/java/school/redrover/NewItemTest.java
@@ -1,5 +1,8 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -267,5 +270,23 @@ public class NewItemTest extends BaseTest {
         Assert.assertTrue(isTypeChecked);
         Assert.assertEquals(currentUrl, String.format("http://localhost:8080/job/%s/configure", PROJECT_NAME));
         Assert.assertTrue(pageHeading.contains(PROJECT_NAME));
+    }
+
+    @DataProvider(name = "unsafeCharacters")
+    public Object [][] unsafeCharacters() {
+        return new Object[][]{
+                {"!"}, {"#"}, {"$"}, {"%"}, {"&"}, {"*"}, {"/"}, {";"}, {":"},
+                {">"}, {"<"}, {"?"}, {"@"}, {"["}, {"\\"}, {"]"}, {"^"}, {"|"}
+        };
+    }
+
+    @Test(dataProvider = "unsafeCharacters")
+    public void testCreateNewItemWithUnsafeCharacterInName(String x) {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(x);
+        WebElement errorMessage = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("itemname-invalid")));
+
+        Assert.assertEquals(errorMessage.getText(),"» ‘" + x + "’ is an unsafe character");
+        Assert.assertEquals(errorMessage.getCssValue("color"), "rgba(255, 0, 0, 1)");
     }
 }
