606369e2d64998fe9e1ef9d13d0420ff1b4ab820
Alex_299 <sir.nevajn@yandex.ru>
2024-05-21 14:08:05 +0300

    delays ware changed in Actions (1100/600)

--- src/test/java/school/redrover/model/FreestyleConfigPage.java
+++ src/test/java/school/redrover/model/FreestyleConfigPage.java
@@ -93,7 +93,7 @@ public class FreestyleConfigPage extends BaseConfigPage<FreestyleProjectPage> {
 
             if (w.isDisplayed()) {
                 ((JavascriptExecutor) getDriver()).executeScript("return arguments[0].scrollIntoView({block:'center'});", w);
-                new Actions(getDriver()).pause(700).moveToElement(w).pause(600).perform();
+                new Actions(getDriver()).pause(1100).moveToElement(w).pause(600).perform();
                 result.add(getDriver().findElement(By.xpath("//div[@class='tippy-box']")).getText());
             }
         }
