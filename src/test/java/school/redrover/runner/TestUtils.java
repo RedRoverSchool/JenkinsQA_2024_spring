b7e66caffa11986d7507e272d5e115e4923e3c14
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-06-09 18:25:21 +0300

    [-] unused imports

--- src/test/java/school/redrover/runner/TestUtils.java
+++ src/test/java/school/redrover/runner/TestUtils.java
@@ -1,18 +1,13 @@
 package school.redrover.runner;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import school.redrover.model.HomePage;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
