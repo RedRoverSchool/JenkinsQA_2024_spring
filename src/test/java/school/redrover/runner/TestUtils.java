90dcd98d48c35541b407c1bc0e97e09c9546090e
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-06-09 19:22:47 +0300

    [!] removed unused things

--- src/test/java/school/redrover/runner/TestUtils.java
+++ src/test/java/school/redrover/runner/TestUtils.java
@@ -1,35 +1,13 @@
 package school.redrover.runner;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import school.redrover.model.HomePage;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 public final class TestUtils {
-
-//    public enum ProjectType {
-//        FREESTYLE_PROJECT("Freestyle project"),
-//        PIPELINE("Pipeline"),
-//        MULTI_CONFIGURATION_PROJECT("Multi-configuration project"),
-//        FOLDER("Folder"),
-//        MULTIBRANCH_PIPELINE("Multibranch Pipeline"),
-//        ORGANIZATION_FOLDER("Organization Folder");
-//
-//        private final String projectTypeName;
-//
-//        public String getProjectTypeName() {
-//            return projectTypeName;
-//        }
-//        ProjectType(String projectTypeName) {
-//            this.projectTypeName = projectTypeName;
-//        }
-//    }
 
     public static class Item {
         public static final String FREESTYLE_PROJECT = "hudson_model_FreeStyleProject";
@@ -39,24 +17,6 @@ public final class TestUtils {
         public static final String MULTI_BRANCH_PIPELINE = "org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject";
         public static final String ORGANIZATION_FOLDER = "jenkins_branch_OrganizationFolder";
     }
-
-//    public static final String PIPELINE = "Pipeline";
-
-//    public static void createItem(String type, String name, BaseTest baseTest) {
-//        baseTest.getDriver().findElement(By.linkText("New Item")).click();
-//        baseTest.getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("name"))).sendKeys(name);
-//        baseTest.getDriver().findElement(By.xpath("//span[text()='" + type + "']")).click();
-//        baseTest.getDriver().findElement(By.id("ok-button")).click();
-//        baseTest.getDriver().findElement(By.xpath("//button[contains(text(), 'Save')]")).click();
-//    }
-
-    public static void goToMainPage(WebDriver driver) {
-        driver.findElement(By.id("jenkins-name-icon")).click();
-    }
-
-//    public static String getUniqueName(String value) {
-//        return value + new SimpleDateFormat("HHmmssSS").format(new Date());
-//    }
 
     public static String asURL(String str) {
         return URLEncoder.encode(str.trim(), StandardCharsets.UTF_8)
@@ -140,20 +100,9 @@ public final class TestUtils {
                 .clickLogo();
     }
 
-//    public static void openElementDropdown(BaseTest baseTest, WebElement element) {
-//        WebElement chevron = element.findElement(By.cssSelector("[class $= 'chevron']"));
-//
-//        ((JavascriptExecutor) baseTest.getDriver()).executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));", chevron);
-//        ((JavascriptExecutor) baseTest.getDriver()).executeScript("arguments[0].dispatchEvent(new Event('click'));", chevron);
-//    }
-
     public static String randomString() {
         return UUID.randomUUID().toString();
     }
-
-//    public static List<String> getTexts(List<WebElement> elementList) {
-//        return elementList.stream().map(WebElement::getText).toList();
-//    }
 
     public static void resetJenkinsTheme(BaseTest baseTest) {
         new HomePage(baseTest.getDriver())
@@ -165,20 +114,10 @@ public final class TestUtils {
 
     public static List<String> getJobsBeginningFromThisFirstLetters(BaseTest baseTest, String firstLetters) {
 
-        return new HomePage(baseTest.getDriver()).getItemList()
+        return new HomePage(baseTest.getDriver())
+                .getItemList()
                 .stream()
                 .filter(el -> el.substring(0, firstLetters.length()).equalsIgnoreCase(firstLetters))
                 .toList();
     }
-
-    public static void assertEqualsLists(List<String> actualList, List<String> expectedList) {
-        try {
-            Assert.assertEquals(actualList, expectedList);
-        } catch (AssertionError e) {
-            Collections.sort(expectedList);
-            Assert.assertEquals(actualList, expectedList,
-                    "Actual list is different after sorting expected values alphabetically");
-        }
-    }
-
 }
