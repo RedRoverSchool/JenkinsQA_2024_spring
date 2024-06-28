b662a317e89de3d05f2f69c5c3f9a91f9e674a6b
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-06-28 20:59:15 +0300

    [+] create | getAllJobs | deleteTwoMethods

--- src/test/java/school/redrover/APIJenkins1Test.java
+++ src/test/java/school/redrover/APIJenkins1Test.java
@@ -4,118 +4,92 @@ import com.google.common.net.HttpHeaders;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseAPITest;
 import school.redrover.runner.ProjectUtils;
+import school.redrover.runner.TestUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
-public class APIJenkins1Test {
-    private String token;
-
-    private static String getBasicAuth(String username, String password) {
-        String valueToEncode = username + ":" + password;
-        return "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
-    }
-
-    @BeforeMethod
-    public void generateNewToken() throws IOException {
-        final BasicCookieStore cookieStore = new BasicCookieStore();
-        final String crumb;
-
-        try (CloseableHttpClient httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore).build()) {
-
-            HttpGet request = new HttpGet(ProjectUtils.getUrl() + "crumbIssuer/api/json");
-
-            request.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuth(ProjectUtils.getUserName(), ProjectUtils.getPassword()));
-
-            try (CloseableHttpResponse response = httpClient.execute(request)) {
-                Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
-
-                System.out.println("all cookies>>>");
-                cookieStore.getCookies().forEach(System.out::println);
-
-
-                crumb = EntityUtils.toString(response.getEntity()).substring(61, 125);
-                System.out.println("crumb>>> " + crumb);
-            }
-
-            HttpPost httpPost = new HttpPost(
-                    ProjectUtils.getUrl() + "me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken");
-
-            httpPost.addHeader("Jenkins-Crumb", crumb);
-            httpPost.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuth(ProjectUtils.getUserName(), ProjectUtils.getPassword()));
-
-            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-                Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
-                String token1 = EntityUtils.toString(response.getEntity());
-                System.out.println("token>>> " + token1);
-                token = token1.substring(153, 187);
-                System.out.println("token>>> " + token);
-            }
-        }
-    }
+public class APIJenkins1Test extends BaseAPITest {
+    private final static String JOB_NAME = "this is the job name two";
 
     @Test
     public void testCreateJob() throws IOException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
 
-            HttpPost httpPost = new HttpPost(
-                    ProjectUtils.getUrl() + "view/all/createItem");
+            HttpPost httpPost = new HttpPost(ProjectUtils.getUrl() + "view/all/createItem/");
 
             final List<NameValuePair> nameValuePairs = new ArrayList<>();
-            nameValuePairs.add(new BasicNameValuePair("name", "this is the job name"));
+            nameValuePairs.add(new BasicNameValuePair("name", JOB_NAME));
             nameValuePairs.add(new BasicNameValuePair("mode", "hudson.model.FreeStyleProject"));
+
             httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            httpPost.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuth(ProjectUtils.getUserName(), token));
+            httpPost.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken());
 
             try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+
+                String jsonString = EntityUtils.toString(response.getEntity());
+                System.out.println(jsonString);
+
                 Assert.assertEquals(response.getStatusLine().getStatusCode(), 302);
             }
         }
     }
 
-    @Test
-    public void testGetJob() throws IOException {
+    @Test(dependsOnMethods = "testCreateJob")
+    public void testGetAllJobs() throws IOException {
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet(ProjectUtils.getUrl() + "api/json");
+            HttpGet request = new HttpGet(ProjectUtils.getUrl() + "api/json/");
 
-            request.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuth(ProjectUtils.getUserName(), token));
+            request.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken());
 
             try (CloseableHttpResponse response = httpClient.execute(request)) {
-                Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
 
                 String jsonString = EntityUtils.toString(response.getEntity());
                 System.out.println(jsonString);
+
+                Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
             }
         }
     }
 
-    @AfterMethod
-    public void revokeAll() throws IOException {
+    @Test(dependsOnMethods = "testGetAllJobs")
+    public void testDeleteJobViaHttpDelete() throws IOException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
 
-            HttpPost httpPost = new HttpPost(
-                    ProjectUtils.getUrl() + "me/descriptorByName/jenkins.security.ApiTokenProperty/revokeAll");
+            HttpDelete httpDelete = new HttpDelete(ProjectUtils.getUrl() + "job/" + TestUtils.asURL(JOB_NAME) + "/");
 
-            httpPost.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuth(ProjectUtils.getUserName(), token));
+            httpDelete.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken());
+
+            try (CloseableHttpResponse response = httpClient.execute(httpDelete)) {
+                Assert.assertEquals(response.getStatusLine().getStatusCode(), 204);
+            }
+        }
+    }
+
+    @Test
+    public void testDeleteJobViaDoDelete() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+
+            HttpPost httpPost = new HttpPost(ProjectUtils.getUrl() + "job/" + TestUtils.asURL(JOB_NAME) + "/doDelete/");
+
+            httpPost.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken());
 
             try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-                Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+                Assert.assertEquals(response.getStatusLine().getStatusCode(), 302);
             }
         }
     }
