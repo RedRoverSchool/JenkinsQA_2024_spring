0f35924cda4277120e68b7c89c7655dc733d017d
AllaBird <allavorobieva@yahoo.com>
2024-07-17 00:07:13 -0400

    Checklist fix

--- src/test/java/school/redrover/runner/ResourceUtils.java
+++ src/test/java/school/redrover/runner/ResourceUtils.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class ResourceUtils {
+public final class ResourceUtils {
     private static String toStringAndClose(InputStream inputStream) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             StringBuilder result = new StringBuilder();
